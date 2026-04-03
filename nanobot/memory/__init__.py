16fe124b95c88d5a9f185f573d5dde822d3704ce
litiejian <litiejian@lixiang.com>
2026-04-03 09:12:52 +0800

    fix(memory): export MemoryStore backward-compat alias from __init__

--- nanobot/memory/__init__.py
+++ nanobot/memory/__init__.py
@@ -1,6 +1,6 @@
 """Memory storage plugin module."""
 
 from nanobot.memory.base import BaseMemoryStore
-from nanobot.memory.store import NormalMemoryStore
+from nanobot.memory.store import MemoryStore, NormalMemoryStore
 
-__all__ = ["BaseMemoryStore", "NormalMemoryStore"]
+__all__ = ["BaseMemoryStore", "MemoryStore", "NormalMemoryStore"]
