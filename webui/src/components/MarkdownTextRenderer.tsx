9ef9fd20ad99263f08f3837f73fbde12fcd1b1f4
chengyongru <chengyongru.ai@gmail.com>
2026-06-08 17:31:10 +0800

    fix(webui): render TeX math delimiters

--- webui/src/components/MarkdownTextRenderer.tsx
+++ webui/src/components/MarkdownTextRenderer.tsx
@@ -26,6 +26,7 @@ import {
 } from "@/components/FileReferenceChip";
 import { inferMediaKind } from "@/lib/media";
 import { faviconUrls } from "@/lib/provider-brand";
+import { remarkTexMath } from "@/lib/remark-tex-math";
 import { cn } from "@/lib/utils";
 
 import "katex/dist/katex.min.css";
@@ -192,6 +193,7 @@ const remarkPlugins: NonNullable<ReactMarkdownOptions["remarkPlugins"]> = [
   remarkBreaks,
   remarkGfm,
   [remarkMath, { singleDollarTextMath: false }],
+  remarkTexMath,
   remarkSafeHtmlSubset,
 ];
 const rehypePlugins: NonNullable<ReactMarkdownOptions["rehypePlugins"]> = [rehypeKatex];
