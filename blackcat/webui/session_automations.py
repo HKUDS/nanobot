nanoinfraorg/nanoinfra

feat(desktop): polish desktop shell and shared WebUI surfaces (#4195)
* feat(desktop): add native host scaffold

* feat(webui): track turns and usage in gateway

* feat(webui): polish desktop chat experience

* feat(apps): add ArcGIS and Joplin logos

* feat(desktop): polish shell and shared surfaces

* fix(webui): avoid preview chips for glob references

* test: align CI expectations for token fallback

* feat(webui): preview prompt rail entries

* feat(webui): add prompt navigator drawer

* style(webui): refine prompt navigator placement

* style(webui): align prompt navigator with header actions

* style(webui): simplify prompt navigator header

* refactor(webui): clean thread resource refresh

* feat(desktop): add native reply notifications

* fix(webui): preserve desktop restart and replay state

* fix(desktop): harden gateway proxy startup

* fix(web): fall back when readability is unavailable

* fix(desktop): hide window instead of closing on macos

* fix(webui): unify desktop header actions

* fix(webui): simplify prompt history rows

* fix(desktop): log notification delivery failures

* chore(desktop): clean source package artifacts

* fix(cron): support one-time relative reminders

* fix(webui): reveal scroll button in place

* Revert "fix(cron): support one-time relative reminders"

This reverts commit 4c4661da120a3c7283e0768412bae48604e7390b.

* refactor(webui): extract token usage heatmap

* docs(desktop): clarify contributor guides

---------

Co-authored-by: chengyongru <2755839590@qq.com>

nanobot/webui/session_automations.py
@@ -1,0 +1,56 @@
+"""Session-scoped automation payloads for the embedded WebUI."""
+
+from __future__ import annotations
+
+from typing import Any, Protocol
+
+from nanobot.cron.types import CronJob
+
+
+class _CronServiceLike(Protocol):
+    def list_jobs(self, *, include_disabled: bool = False) -> list[CronJob]: ...
+
+
+def session_automations_payload(
+    cron_service: _CronServiceLike | None,
+    session_key: str,
+) -> dict[str, Any]:
+    """Return user-created automation jobs attached to a WebUI session."""
+    jobs: list[CronJob] = []
+    if cron_service is not None:
+        all_jobs = cron_service.list_jobs(include_disabled=True)
+        jobs = [job for job in all_jobs if _job_matches_session(job, session_key)]
+    return {"jobs": [_serialize_job(job) for job in jobs]}
+
+
+def _job_matches_session(job: CronJob, session_key: str) -> bool:
+    payload = job.payload
+    if payload.kind != "agent_turn":
+        return False
+    if payload.session_key:
+        return payload.session_key == session_key
+    if payload.channel and payload.to:
+        return f"{payload.channel}:{payload.to}" == session_key
+    return False
+
+
+def _serialize_job(job: CronJob) -> dict[str, Any]:
+    return {
+        "id": job.id,
+        "name": job.name,
+        "enabled": job.enabled,
+        "schedule": {
+            "kind": job.schedule.kind,
+            "at_ms": job.schedule.at_ms,
+            "every_ms": job.schedule.every_ms,
+            "expr": job.schedule.expr,
+            "tz": job.schedule.tz,
+        },
+        "payload": {
+            "message": job.payload.message,
+        },
+        "state": {
+            "next_run_at_ms": job.state.next_run_at_ms,
+            "last_status": job.state.last_status,
+        },
+    }

nanobot/webui/skills_api.py
@@ -1,0 +1,61 @@
+"""Lightweight skill summaries for the WebUI."""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any
+
+from nanobot.agent.skills import SkillsLoader
+
+
+def webui_skills_payload(
+    workspace_path: Path,
+    *,
+    disabled_skills: set[str] | None = None,
+) -> dict[str, Any]:
+    """Return agent skills without leaking local filesystem paths."""
+    loader = SkillsLoader(workspace_path, disabled_skills=disabled_skills)
+    entries = sorted(
+        loader.list_skills(filter_unavailable=False),
+        key=lambda entry: (entry.get("source") != "workspace", entry["name"]),
+    )
+    return {"skills": [_skill_payload(loader, entry) for entry in entries]}
+
+
+def webui_skill_detail_payload(
+    workspace_path: Path,
+    name: str,
+    *,
+    disabled_skills: set[str] | None = None,
+) -> dict[str, Any] | None:
+    """Return a single skill's safe detail payload."""
+    loader = SkillsLoader(workspace_path, disabled_skills=disabled_skills)
+    entries = loader.list_skills(filter_unavailable=False)
+    entry = next((item for item in entries if item["name"] == name), None)
+    if entry is None:
+        return None
+    return {
+        **_skill_payload(loader, entry),
+        "requirements": loader.get_skill_requirements(name),
+        "raw_markdown": loader.load_skill(name) or "",
+    }
+
+
+def _skill_payload(loader: SkillsLoader, entry: dict[str, str]) -> dict[str, Any]:
+    name = entry["name"]
+    metadata = loader.get_skill_metadata(name)
+    available, unavailable_reason = loader.get_skill_availability(name)
+    return {
+        "name": name,
+        "description": _description(metadata, name),
+        "source": entry.get("source", "unknown"),
+        "available": available,
+        "unavailable_reason": unavailable_reason,
+    }
+
+
+def _description(metadata: dict[str, Any] | None, fallback: str) -> str:
+    if metadata is None:
+        return fallback
+    value = metadata.get("description")
+    return value.strip() if isinstance(value, str) and value.strip() else fallback

tests/cli/test_commands.py
@@ -9,15 +9,35 @@ import pytest
 from typer.testing import CliRunner
 
 from nanobot.bus.events import OutboundMessage
-from nanobot.cli.commands import app
-from nanobot.providers.factory import make_provider
+from nanobot.cli.commands import _proactive_delivery_metadata, app
 from nanobot.config.schema import Config
 from nanobot.cron.types import CronJob, CronPayload
-from nanobot.providers.factory import ProviderSnapshot
+from nanobot.providers.factory import ProviderSnapshot, make_provider
 from nanobot.providers.openai_codex_provider import _strip_model_prefix
 from nanobot.providers.registry import find_by_name
 
 runner = CliRunner()
+
+
+def test_proactive_websocket_delivery_gets_fresh_turn_id() -> None:
+    metadata = {
+        "webui": True,
+        "webui_turn_id": "turn-that-created-the-reminder",
+        "workspace_scope": {"mode": "default"},
+    }
+
+    out = _proactive_delivery_metadata(
+        "websocket",
+        metadata,
+        turn_seed="cron:drink-water",
+        source_label="drink water",
+    )
+
+    assert out["webui"] is True
+    assert out["workspace_scope"] == {"mode": "default"}
+    assert out["webui_turn_id"].startswith("cron:drink-water:")
+    assert out["webui_turn_id"] != metadata["webui_turn_id"]
+    assert out["_webui_message_source"] == {"kind": "cron", "label": "drink water"}
 
 
 def _fake_provider():
@@ -542,8 +562,8 @@ def test_openai_compat_provider_passes_model_through():
 
 
 def test_make_provider_uses_github_copilot_backend():
-    from nanobot.providers.factory import make_provider
     from nanobot.config.schema import Config
+    from nanobot.providers.factory import make_provider
 
     config = Config.model_validate(
         {
@@ -1317,6 +1337,41 @@ def test_gateway_cron_evaluator_receives_scheduled_reminder_context(
         }
     ]
 
+    bus.publish_outbound.reset_mock()
+    old_turn_id = "turn-that-created-the-reminder"
+    websocket_job = CronJob(
+        id="drink-water",
+        name="drink water",
+        payload=CronPayload(
+            message="Remind me to drink water.",
+            deliver=True,
+            channel="websocket",
+            to="chat-1",
+            channel_meta={
+                "webui": True,
+                "webui_turn_id": old_turn_id,
+                "workspace_scope": {"mode": "default"},
+            },
+            session_key="websocket:chat-1",
+        ),
+    )
+
+    response = asyncio.run(cron.on_job(websocket_job))
+
+    assert response == "Time to stretch."
+    bus.publish_outbound.assert_awaited_once()
+    delivered = bus.publish_outbound.await_args.args[0]
+    assert delivered.channel == "websocket"
+    assert delivered.chat_id == "chat-1"
+    assert delivered.metadata["webui"] is True
+    assert delivered.metadata["workspace_scope"] == {"mode": "default"}
+    assert delivered.metadata["webui_turn_id"].startswith("cron:drink-water:")
+    assert delivered.metadata["webui_turn_id"] != old_turn_id
+    assert delivered.metadata["_webui_message_source"] == {
+        "kind": "cron",
+        "label": "drink water",
+    }
+
 
 def test_gateway_cron_job_suppresses_intermediate_progress(
     monkeypatch, tmp_path: Path
@@ -1597,6 +1652,65 @@ def test_configure_desktop_gateway_forces_local_websocket_only() -> None:
     assert extras["websocket"]["unix_socket_path"] == "/tmp/nanobot-test.sock"
     assert extras["websocket"]["token_issue_secret"] == "secret"
     assert extras["websocket"]["websocket_requires_token"] is True
+
+
+def test_load_or_create_desktop_config_bootstraps_without_api_key(tmp_path: Path) -> None:
+    from nanobot.cli.commands import _load_or_create_desktop_config
+
+    config_path = tmp_path / "config.json"
+    loaded = _load_or_create_desktop_config(
+        str(config_path),
+        str(tmp_path / "workspace"),
+    )
+
+    assert loaded.agents.defaults.provider == "openai_codex"
+    assert loaded.agents.defaults.model == "openai-codex/gpt-5.1-codex"
+    assert loaded.agents.defaults.model_preset is None
+    assert config_path.exists()
+    saved = json.loads(config_path.read_text(encoding="utf-8"))
+    assert saved["agents"]["defaults"]["provider"] == ""
+    assert saved["agents"]["defaults"]["model"] == ""
+    assert make_provider(loaded).get_default_model() == "openai-codex/gpt-5.1-codex"
+
+
+def test_load_or_create_desktop_config_repairs_existing_unconfigured_default(
+    tmp_path: Path,
+) -> None:
+    from nanobot.cli.commands import _load_or_create_desktop_config
+    from nanobot.config.loader import save_config
+
+    config_path = tmp_path / "config.json"
+    save_config(Config(), config_path)
+
+    loaded = _load_or_create_desktop_config(str(config_path), None)
+    saved = json.loads(config_path.read_text(encoding="utf-8"))
+
+    assert loaded.agents.defaults.provider == "openai_codex"
+    assert loaded.agents.defaults.model == "openai-codex/gpt-5.1-codex"
+    assert saved["agents"]["defaults"]["provider"] == ""
+    assert saved["agents"]["defaults"]["model"] == ""
+    assert make_provider(loaded).get_default_model() == "openai-codex/gpt-5.1-codex"
+
+
+def test_load_or_create_desktop_config_unwinds_persisted_bootstrap(
+    tmp_path: Path,
+) -> None:
+    from nanobot.cli.commands import _load_or_create_desktop_config
+    from nanobot.config.loader import save_config
+
+    config_path = tmp_path / "config.json"
+    config = Config()
+    config.agents.defaults.provider = "openai_codex"
+    config.agents.defaults.model = "openai-codex/gpt-5.1-codex"
+    save_config(config, config_path)
+
+    loaded = _load_or_create_desktop_config(str(config_path), None)
+    saved = json.loads(config_path.read_text(encoding="utf-8"))
+
+    assert loaded.agents.defaults.provider == "openai_codex"
+    assert loaded.agents.defaults.model == "openai-codex/gpt-5.1-codex"
+    assert saved["agents"]["defaults"]["provider"] == ""
+    assert saved["agents"]["defaults"]["model"] == ""
 
 
 def test_gateway_health_endpoint_binds_and_serves_expected_responses(

HKUDS/nanobot

blackcat/webui/session_automations.py
@@ -1,0 +1,56 @@
+"""Session-scoped automation payloads for the embedded WebUI."""
+
+from __future__ import annotations
+
+from typing import Any, Protocol
+
+from blackcat.cron.types import CronJob
+
+
+class _CronServiceLike(Protocol):
+    def list_jobs(self, *, include_disabled: bool = False) -> list[CronJob]: ...
+
+
+def session_automations_payload(
+    cron_service: _CronServiceLike | None,
+    session_key: str,
+) -> dict[str, Any]:
+    """Return user-created automation jobs attached to a WebUI session."""
+    jobs: list[CronJob] = []
+    if cron_service is not None:
+        all_jobs = cron_service.list_jobs(include_disabled=True)
+        jobs = [job for job in all_jobs if _job_matches_session(job, session_key)]
+    return {"jobs": [_serialize_job(job) for job in jobs]}
+
+
+def _job_matches_session(job: CronJob, session_key: str) -> bool:
+    payload = job.payload
+    if payload.kind != "agent_turn":
+        return False
+    if payload.session_key:
+        return payload.session_key == session_key
+    if payload.channel and payload.to:
+        return f"{payload.channel}:{payload.to}" == session_key
+    return False
+
+
+def _serialize_job(job: CronJob) -> dict[str, Any]:
+    return {
+        "id": job.id,
+        "name": job.name,
+        "enabled": job.enabled,
+        "schedule": {
+            "kind": job.schedule.kind,
+            "at_ms": job.schedule.at_ms,
+            "every_ms": job.schedule.every_ms,
+            "expr": job.schedule.expr,
+            "tz": job.schedule.tz,
+        },
+        "payload": {
+            "message": job.payload.message,
+        },
+        "state": {
+            "next_run_at_ms": job.state.next_run_at_ms,
+            "last_status": job.state.last_status,
+        },
+    }

blackcat/webui/skills_api.py
@@ -1,0 +1,61 @@
+"""Lightweight skill summaries for the WebUI."""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any
+
+from blackcat.agent.skills import SkillsLoader
+
+
+def webui_skills_payload(
+    workspace_path: Path,
+    *,
+    disabled_skills: set[str] | None = None,
+) -> dict[str, Any]:
+    """Return agent skills without leaking local filesystem paths."""
+    loader = SkillsLoader(workspace_path, disabled_skills=disabled_skills)
+    entries = sorted(
+        loader.list_skills(filter_unavailable=False),
+        key=lambda entry: (entry.get("source") != "workspace", entry["name"]),
+    )
+    return {"skills": [_skill_payload(loader, entry) for entry in entries]}
+
+
+def webui_skill_detail_payload(
+    workspace_path: Path,
+    name: str,
+    *,
+    disabled_skills: set[str] | None = None,
+) -> dict[str, Any] | None:
+    """Return a single skill's safe detail payload."""
+    loader = SkillsLoader(workspace_path, disabled_skills=disabled_skills)
+    entries = loader.list_skills(filter_unavailable=False)
+    entry = next((item for item in entries if item["name"] == name), None)
+    if entry is None:
+        return None
+    return {
+        **_skill_payload(loader, entry),
+        "requirements": loader.get_skill_requirements(name),
+        "raw_markdown": loader.load_skill(name) or "",
+    }
+
+
+def _skill_payload(loader: SkillsLoader, entry: dict[str, str]) -> dict[str, Any]:
+    name = entry["name"]
+    metadata = loader.get_skill_metadata(name)
+    available, unavailable_reason = loader.get_skill_availability(name)
+    return {
+        "name": name,
+        "description": _description(metadata, name),
+        "source": entry.get("source", "unknown"),
+        "available": available,
+        "unavailable_reason": unavailable_reason,
+    }
+
+
+def _description(metadata: dict[str, Any] | None, fallback: str) -> str:
+    if metadata is None:
+        return fallback
+    value = metadata.get("description")
+    return value.strip() if isinstance(value, str) and value.strip() else fallback

tests/cli/test_commands.py
@@ -15,8 +15,30 @@ from blackcat.cron.types import CronJob, CronPayload
 from blackcat.providers.factory import ProviderSnapshot, make_provider
 from blackcat.providers.openai_codex_provider import _strip_model_prefix
 from blackcat.providers.registry import find_by_name
+from blackcat.cli.commands import _proactive_delivery_metadata, app
 
 runner = CliRunner()
+
+
+def test_proactive_websocket_delivery_gets_fresh_turn_id() -> None:
+    metadata = {
+        "webui": True,
+        "webui_turn_id": "turn-that-created-the-reminder",
+        "workspace_scope": {"mode": "default"},
+    }
+
+    out = _proactive_delivery_metadata(
+        "websocket",
+        metadata,
+        turn_seed="cron:drink-water",
+        source_label="drink water",
+    )
+
+    assert out["webui"] is True
+    assert out["workspace_scope"] == {"mode": "default"}
+    assert out["webui_turn_id"].startswith("cron:drink-water:")
+    assert out["webui_turn_id"] != metadata["webui_turn_id"]
+    assert out["_webui_message_source"] == {"kind": "cron", "label": "drink water"}
 
 
 def _fake_provider():
@@ -1316,6 +1338,41 @@ def test_gateway_cron_evaluator_receives_scheduled_reminder_context(
         }
     ]
 
+    bus.publish_outbound.reset_mock()
+    old_turn_id = "turn-that-created-the-reminder"
+    websocket_job = CronJob(
+        id="drink-water",
+        name="drink water",
+        payload=CronPayload(
+            message="Remind me to drink water.",
+            deliver=True,
+            channel="websocket",
+            to="chat-1",
+            channel_meta={
+                "webui": True,
+                "webui_turn_id": old_turn_id,
+                "workspace_scope": {"mode": "default"},
+            },
+            session_key="websocket:chat-1",
+        ),
+    )
+
+    response = asyncio.run(cron.on_job(websocket_job))
+
+    assert response == "Time to stretch."
+    bus.publish_outbound.assert_awaited_once()
+    delivered = bus.publish_outbound.await_args.args[0]
+    assert delivered.channel == "websocket"
+    assert delivered.chat_id == "chat-1"
+    assert delivered.metadata["webui"] is True
+    assert delivered.metadata["workspace_scope"] == {"mode": "default"}
+    assert delivered.metadata["webui_turn_id"].startswith("cron:drink-water:")
+    assert delivered.metadata["webui_turn_id"] != old_turn_id
+    assert delivered.metadata["_webui_message_source"] == {
+        "kind": "cron",
+        "label": "drink water",
+    }
+
 
 def test_gateway_cron_job_suppresses_intermediate_progress(
     monkeypatch, tmp_path: Path
@@ -1596,6 +1653,65 @@ def test_configure_desktop_gateway_forces_local_websocket_only() -> None:
     assert extras["websocket"]["unix_socket_path"] == "/tmp/blackcat-test.sock"
     assert extras["websocket"]["token_issue_secret"] == "secret"
     assert extras["websocket"]["websocket_requires_token"] is True
+
+
+def test_load_or_create_desktop_config_bootstraps_without_api_key(tmp_path: Path) -> None:
+    from blackcat.cli.commands import _load_or_create_desktop_config
+
+    config_path = tmp_path / "config.json"
+    loaded = _load_or_create_desktop_config(
+        str(config_path),
+        str(tmp_path / "workspace"),
+    )
+
+    assert loaded.agents.defaults.provider == "openai_codex"
+    assert loaded.agents.defaults.model == "openai-codex/gpt-5.1-codex"
+    assert loaded.agents.defaults.model_preset is None
+    assert config_path.exists()
+    saved = json.loads(config_path.read_text(encoding="utf-8"))
+    assert saved["agents"]["defaults"]["provider"] == ""
+    assert saved["agents"]["defaults"]["model"] == ""
+    assert make_provider(loaded).get_default_model() == "openai-codex/gpt-5.1-codex"
+
+
+def test_load_or_create_desktop_config_repairs_existing_unconfigured_default(
+    tmp_path: Path,
+) -> None:
+    from blackcat.cli.commands import _load_or_create_desktop_config
+    from blackcat.config.loader import save_config
+
+    config_path = tmp_path / "config.json"
+    save_config(Config(), config_path)
+
+    loaded = _load_or_create_desktop_config(str(config_path), None)
+    saved = json.loads(config_path.read_text(encoding="utf-8"))
+
+    assert loaded.agents.defaults.provider == "openai_codex"
+    assert loaded.agents.defaults.model == "openai-codex/gpt-5.1-codex"
+    assert saved["agents"]["defaults"]["provider"] == ""
+    assert saved["agents"]["defaults"]["model"] == ""
+    assert make_provider(loaded).get_default_model() == "openai-codex/gpt-5.1-codex"
+
+
+def test_load_or_create_desktop_config_unwinds_persisted_bootstrap(
+    tmp_path: Path,
+) -> None:
+    from blackcat.cli.commands import _load_or_create_desktop_config
+    from blackcat.config.loader import save_config
+
+    config_path = tmp_path / "config.json"
+    config = Config()
+    config.agents.defaults.provider = "openai_codex"
+    config.agents.defaults.model = "openai-codex/gpt-5.1-codex"
+    save_config(config, config_path)
+
+    loaded = _load_or_create_desktop_config(str(config_path), None)
+    saved = json.loads(config_path.read_text(encoding="utf-8"))
+
+    assert loaded.agents.defaults.provider == "openai_codex"
+    assert loaded.agents.defaults.model == "openai-codex/gpt-5.1-codex"
+    assert saved["agents"]["defaults"]["provider"] == ""
+    assert saved["agents"]["defaults"]["model"] == ""
 
 
 def test_gateway_health_endpoint_binds_and_serves_expected_responses(