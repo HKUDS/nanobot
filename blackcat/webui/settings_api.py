nanoinfraorg/nanoinfra

feat(transcription): add shared voice input support (#4232)
* feat(webui): add voice transcription input

* feat(webui): render ANSI output in code blocks

* refactor(webui): isolate voice recorder logic

* refactor(transcription): keep websocket ingress thin

* refactor(transcription): resolve channel audio settings on demand

* style(webui): neutralize voice waveform color

* feat(webui): add voice input tooltip

* feat(webui): add voice input keyboard shortcut

* fix(webui): distinguish voice shortcut platforms

* fix(webui): place voice button after model selector

* refactor(webui): share voice hold recording helpers

* fix(desktop): allow microphone voice input

* fix(webui): stabilize token usage month labels

* feat(webui): show voice input on settings overview

* fix(webui): label voice capability as recognition

* fix(webui): align capability overview status

* refactor(webui): isolate transcription socket handling

* fix(webui): soften silent voice waveform

* refactor(audio): clarify transcription service location

* docs(transcription): clarify audio and provider boundaries

* fix(exec): reduce session output polling flake

nanobot/webui/settings_api.py
@@ -15,6 +15,7 @@ from zoneinfo import ZoneInfo
 
 import httpx
 
+from nanobot.audio.transcription import resolve_transcription_config
 from nanobot.config.loader import get_config_path, load_config, save_config
 from nanobot.config.schema import ModelPresetConfig
 from nanobot.providers.image_generation import (
@@ -90,6 +91,7 @@ _IMAGE_GENERATION_ASPECT_RATIOS = {
     "2:3",
     "21:9",
 }
+_TRANSCRIPTION_PROVIDERS = ("groq", "openai")
 _CONTEXT_WINDOW_TOKEN_OPTIONS = {65_536, 262_144}
 _MODEL_CONFIGURATION_SLUG_RE = re.compile(r"[^a-z0-9_-]+")
 _ENV_REF_RE = re.compile(r"\$\{([A-Za-z_][A-Za-z0-9_]*)\}")
@@ -576,6 +578,22 @@ def _image_generation_provider_rows(config: Any) -> list[dict[str, Any]]:
     return rows
 
 
+def _transcription_provider_rows(config: Any) -> list[dict[str, Any]]:
+    rows: list[dict[str, Any]] = []
+    for name in _TRANSCRIPTION_PROVIDERS:
+        spec = find_by_name(name)
+        provider_config = getattr(config.providers, name, None)
+        rows.append({
+            "name": name,
+            "label": spec.label if spec is not None else name,
+            "configured": bool(getattr(provider_config, "api_key", None)),
+            "api_key_hint": _mask_secret_hint(getattr(provider_config, "api_key", None)),
+            "api_base": getattr(provider_config, "api_base", None),
+            "default_api_base": spec.default_api_base if spec and spec.default_api_base else None,
+        })
+    return rows
+
+
 def settings_payload(
     *,
     requires_restart: bool = False,
@@ -633,6 +651,7 @@ def settings_payload(
 
     search_config = config.tools.web.search
     image_config = config.tools.image_generation
+    transcription = resolve_transcription_config(config)
     search_provider = (
         search_config.provider
         if search_config.provider in _WEB_SEARCH_PROVIDER_BY_NAME
@@ -732,6 +751,16 @@ def settings_payload(
             "max_images_per_turn": image_config.max_images_per_turn,
             "save_dir": image_config.save_dir,
             "providers": image_providers,
+        },
+        "transcription": {
+            "enabled": transcription.enabled,
+            "provider": transcription.provider,
+            "provider_configured": transcription.configured,
+            "model": transcription.model,
+            "language": transcription.language,
+            "max_duration_sec": transcription.max_duration_sec,
+            "max_upload_mb": transcription.max_upload_mb,
+            "providers": _transcription_provider_rows(config),
         },
         "runtime": {
             "config_path": str(get_config_path().expanduser()),
@@ -1311,3 +1340,71 @@ def update_image_generation_settings(query: QueryParams) -> dict[str, Any]:
     if changed:
         save_config(config)
     return settings_payload(requires_restart=changed)
+
+
+def update_transcription_settings(query: QueryParams) -> dict[str, Any]:
+    config = load_config()
+    transcription = config.transcription
+    changed = False
+
+    enabled = _query_first(query, "enabled")
+    if enabled is not None:
+        parsed_enabled = _parse_bool(enabled, "enabled")
+        if transcription.enabled != parsed_enabled:
+            transcription.enabled = parsed_enabled
+            changed = True
+
+    provider = _query_first(query, "provider")
+    if provider is not None:
+        provider = provider.strip().lower()
+        if provider not in _TRANSCRIPTION_PROVIDERS:
+            raise WebUISettingsError("unknown transcription provider")
+        if transcription.provider != provider:
+            transcription.provider = provider  # type: ignore[assignment]
+            changed = True
+
+    model = _query_first(query, "model")
+    if model is not None:
+        model = model.strip() or None
+        if model is not None and len(model) > 200:
+            raise WebUISettingsError("transcription model is too long")
+        if transcription.model != model:
+            transcription.model = model
+            changed = True
+
+    language = _query_first(query, "language")
+    if language is not None:
+        language = language.strip().lower() or None
+        if language is not None and not re.fullmatch(r"[a-z]{2,3}", language):
+            raise WebUISettingsError("transcription language must be 2-3 lowercase letters")
+        if transcription.language != language:
+            transcription.language = language
+            changed = True
+
+    max_duration_sec = _query_first_alias(query, "max_duration_sec", "maxDurationSec")
+    if max_duration_sec is not None:
+        try:
+            parsed_duration = int(max_duration_sec)
+        except ValueError:
+            raise WebUISettingsError("max_duration_sec must be an integer") from None
+        if parsed_duration < 1 or parsed_duration > 600:
+            raise WebUISettingsError("max_duration_sec must be between 1 and 600")
+        if transcription.max_duration_sec != parsed_duration:
+            transcription.max_duration_sec = parsed_duration
+            changed = True
+
+    max_upload_mb = _query_first_alias(query, "max_upload_mb", "maxUploadMb")
+    if max_upload_mb is not None:
+        try:
+            parsed_upload = int(max_upload_mb)
+        except ValueError:
+            raise WebUISettingsError("max_upload_mb must be an integer") from None
+        if parsed_upload < 1 or parsed_upload > 100:
+            raise WebUISettingsError("max_upload_mb must be between 1 and 100")
+        if transcription.max_upload_mb != parsed_upload:
+            transcription.max_upload_mb = parsed_upload
+            changed = True
+
+    if changed:
+        save_config(config)
+    return settings_payload()

nanobot/webui/transcription_ws.py
@@ -1,0 +1,46 @@
+"""WebUI transcription envelope handling.
+
+The WebSocket channel owns transport and subscription fan-out. This module owns
+the WebUI-specific audio transcription action carried over that socket.
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+from nanobot.audio.transcription import (
+    TranscriptionIngressError,
+    resolve_transcription_config,
+    transcribe_audio_data_url,
+)
+from nanobot.config.loader import load_config
+
+_MAX_REQUEST_ID_LENGTH = 80
+
+
+async def webui_transcription_event(envelope: dict[str, Any]) -> tuple[str, dict[str, Any]]:
+    """Return the WS event name and payload for one WebUI transcription request."""
+    request_id = envelope.get("request_id")
+    valid_request_id = (
+        isinstance(request_id, str)
+        and 0 < len(request_id) <= _MAX_REQUEST_ID_LENGTH
+    )
+
+    def error(detail: str, **extra: Any) -> tuple[str, dict[str, Any]]:
+        payload: dict[str, Any] = {"detail": detail, **extra}
+        if valid_request_id:
+            payload["request_id"] = request_id
+        return "transcription_error", payload
+
+    if not valid_request_id:
+        return error("invalid_request")
+
+    try:
+        text = await transcribe_audio_data_url(
+            envelope.get("data_url"),
+            resolve_transcription_config(load_config()),
+            duration_ms=envelope.get("duration_ms"),
+        )
+    except TranscriptionIngressError as exc:
+        return error(exc.detail, **exc.extra)
+    return "transcription_result", {"request_id": request_id, "text": text}

tests/providers/test_transcription.py
@@ -8,6 +8,8 @@ from unittest.mock import AsyncMock, patch
 import httpx
 import pytest
 
+from nanobot.audio.transcription import resolve_transcription_config
+from nanobot.config.schema import Config
 from nanobot.providers.transcription import (
     GroqTranscriptionProvider,
     OpenAITranscriptionProvider,
@@ -31,6 +33,65 @@ def _raw_response(status: int, content: bytes) -> httpx.Response:
     """Build a Response with a raw, possibly-malformed body (bypasses json= encoding)."""
     request = httpx.Request("POST", "https://example.test/audio/transcriptions")
     return httpx.Response(status_code=status, content=content, request=request)
+
+
+def test_resolver_uses_legacy_channel_provider_when_top_level_is_unset() -> None:
+    config = Config()
+    config.channels.transcription_provider = "openai"
+    config.channels.transcription_language = "en"
+    config.providers.openai.api_key = "sk-test"
+    config.providers.openai.api_base = "https://proxy.example/v1"
+
+    resolved = resolve_transcription_config(config)
+
+    assert resolved.provider == "openai"
+    assert resolved.model == "whisper-1"
+    assert resolved.language == "en"
+    assert resolved.api_key == "sk-test"
+    assert resolved.api_base == "https://proxy.example/v1"
+    assert resolved.configured is True
+
+
+def test_resolver_prefers_top_level_transcription_over_legacy_channels() -> None:
+    config = Config()
+    config.channels.transcription_provider = "openai"
+    config.channels.transcription_language = "en"
+    config.transcription.provider = "groq"
+    config.transcription.model = "whisper-large-v3-turbo"
+    config.transcription.language = "ko"
+    config.providers.groq.api_key = "gsk-test"
+    config.providers.groq.api_base = "https://groq.example/openai/v1"
+
+    resolved = resolve_transcription_config(config)
+
+    assert resolved.provider == "groq"
+    assert resolved.model == "whisper-large-v3-turbo"
+    assert resolved.language == "ko"
+    assert resolved.api_key == "gsk-test"
+    assert resolved.api_base == "https://groq.example/openai/v1"
+
+
+def test_resolved_transcription_repr_hides_api_key() -> None:
+    config = Config()
+    config.providers.groq.api_key = "gsk-secret"
+
+    resolved = resolve_transcription_config(config)
+
+    assert "gsk-secret" not in repr(resolved)
+    assert "api_key" not in repr(resolved)
+
+
+def test_resolver_keeps_enabled_and_limits_on_effective_config() -> None:
+    config = Config()
+    config.transcription.enabled = False
+    config.transcription.max_duration_sec = 45
+    config.transcription.max_upload_mb = 12
+
+    resolved = resolve_transcription_config(config)
+
+    assert resolved.enabled is False
+    assert resolved.max_duration_sec == 45
+    assert resolved.max_upload_mb == 12
 
 
 # ---------------------------------------------------------------------------
@@ -213,6 +274,32 @@ async def test_provider_omits_language_when_unset(
     assert post.await_count == 1
     files = post.await_args_list[0].kwargs["files"]
     assert "language" not in files
+
+
+@pytest.mark.asyncio
+async def test_provider_forwards_custom_model_in_multipart(audio_file: Path) -> None:
+    provider = GroqTranscriptionProvider(api_key="k", model="whisper-large-v3-turbo")
+    post = AsyncMock(return_value=_response(200, {"text": "ok"}))
+    with patch("httpx.AsyncClient.post", post), patch("asyncio.sleep", AsyncMock()):
+        result = await provider.transcribe(audio_file)
+
+    assert result == "ok"
+    files = post.await_args_list[0].kwargs["files"]
+    assert files["model"] == (None, "whisper-large-v3-turbo")
+
+
+@pytest.mark.asyncio
+async def test_provider_forwards_file_mime_type(tmp_path: Path) -> None:
+    audio = tmp_path / "voice.webm"
+    audio.write_bytes(b"audio")
+    provider = GroqTranscriptionProvider(api_key="k")
+    post = AsyncMock(return_value=_response(200, {"text": "ok"}))
+    with patch("httpx.AsyncClient.post", post), patch("asyncio.sleep", AsyncMock()):
+        result = await provider.transcribe(audio)
+
+    assert result == "ok"
+    files = post.await_args_list[0].kwargs["files"]
+    assert files["file"] == ("voice.webm", b"audio", "audio/webm")
 
 
 @pytest.mark.asyncio

HKUDS/nanobot

blackcat/webui/settings_api.py
@@ -18,6 +18,7 @@ import httpx
 from blackcat.config.loader import get_config_path, load_config, save_config
 from blackcat.config.schema import ModelPresetConfig
 from blackcat.providers.image_generation import (
+from blackcat.audio.transcription import resolve_transcription_config
     get_image_gen_provider,
     image_gen_provider_names,
 )
@@ -90,6 +91,7 @@ _IMAGE_GENERATION_ASPECT_RATIOS = {
     "2:3",
     "21:9",
 }
+_TRANSCRIPTION_PROVIDERS = ("groq", "openai")
 _CONTEXT_WINDOW_TOKEN_OPTIONS = {65_536, 262_144}
 _MODEL_CONFIGURATION_SLUG_RE = re.compile(r"[^a-z0-9_-]+")
 _ENV_REF_RE = re.compile(r"\$\{([A-Za-z_][A-Za-z0-9_]*)\}")
@@ -576,6 +578,22 @@ def _image_generation_provider_rows(config: Any) -> list[dict[str, Any]]:
     return rows
 
 
+def _transcription_provider_rows(config: Any) -> list[dict[str, Any]]:
+    rows: list[dict[str, Any]] = []
+    for name in _TRANSCRIPTION_PROVIDERS:
+        spec = find_by_name(name)
+        provider_config = getattr(config.providers, name, None)
+        rows.append({
+            "name": name,
+            "label": spec.label if spec is not None else name,
+            "configured": bool(getattr(provider_config, "api_key", None)),
+            "api_key_hint": _mask_secret_hint(getattr(provider_config, "api_key", None)),
+            "api_base": getattr(provider_config, "api_base", None),
+            "default_api_base": spec.default_api_base if spec and spec.default_api_base else None,
+        })
+    return rows
+
+
 def settings_payload(
     *,
     requires_restart: bool = False,
@@ -633,6 +651,7 @@ def settings_payload(
 
     search_config = config.tools.web.search
     image_config = config.tools.image_generation
+    transcription = resolve_transcription_config(config)
     search_provider = (
         search_config.provider
         if search_config.provider in _WEB_SEARCH_PROVIDER_BY_NAME
@@ -732,6 +751,16 @@ def settings_payload(
             "max_images_per_turn": image_config.max_images_per_turn,
             "save_dir": image_config.save_dir,
             "providers": image_providers,
+        },
+        "transcription": {
+            "enabled": transcription.enabled,
+            "provider": transcription.provider,
+            "provider_configured": transcription.configured,
+            "model": transcription.model,
+            "language": transcription.language,
+            "max_duration_sec": transcription.max_duration_sec,
+            "max_upload_mb": transcription.max_upload_mb,
+            "providers": _transcription_provider_rows(config),
         },
         "runtime": {
             "config_path": str(get_config_path().expanduser()),
@@ -1311,3 +1340,71 @@ def update_image_generation_settings(query: QueryParams) -> dict[str, Any]:
     if changed:
         save_config(config)
     return settings_payload(requires_restart=changed)
+
+
+def update_transcription_settings(query: QueryParams) -> dict[str, Any]:
+    config = load_config()
+    transcription = config.transcription
+    changed = False
+
+    enabled = _query_first(query, "enabled")
+    if enabled is not None:
+        parsed_enabled = _parse_bool(enabled, "enabled")
+        if transcription.enabled != parsed_enabled:
+            transcription.enabled = parsed_enabled
+            changed = True
+
+    provider = _query_first(query, "provider")
+    if provider is not None:
+        provider = provider.strip().lower()
+        if provider not in _TRANSCRIPTION_PROVIDERS:
+            raise WebUISettingsError("unknown transcription provider")
+        if transcription.provider != provider:
+            transcription.provider = provider  # type: ignore[assignment]
+            changed = True
+
+    model = _query_first(query, "model")
+    if model is not None:
+        model = model.strip() or None
+        if model is not None and len(model) > 200:
+            raise WebUISettingsError("transcription model is too long")
+        if transcription.model != model:
+            transcription.model = model
+            changed = True
+
+    language = _query_first(query, "language")
+    if language is not None:
+        language = language.strip().lower() or None
+        if language is not None and not re.fullmatch(r"[a-z]{2,3}", language):
+            raise WebUISettingsError("transcription language must be 2-3 lowercase letters")
+        if transcription.language != language:
+            transcription.language = language
+            changed = True
+
+    max_duration_sec = _query_first_alias(query, "max_duration_sec", "maxDurationSec")
+    if max_duration_sec is not None:
+        try:
+            parsed_duration = int(max_duration_sec)
+        except ValueError:
+            raise WebUISettingsError("max_duration_sec must be an integer") from None
+        if parsed_duration < 1 or parsed_duration > 600:
+            raise WebUISettingsError("max_duration_sec must be between 1 and 600")
+        if transcription.max_duration_sec != parsed_duration:
+            transcription.max_duration_sec = parsed_duration
+            changed = True
+
+    max_upload_mb = _query_first_alias(query, "max_upload_mb", "maxUploadMb")
+    if max_upload_mb is not None:
+        try:
+            parsed_upload = int(max_upload_mb)
+        except ValueError:
+            raise WebUISettingsError("max_upload_mb must be an integer") from None
+        if parsed_upload < 1 or parsed_upload > 100:
+            raise WebUISettingsError("max_upload_mb must be between 1 and 100")
+        if transcription.max_upload_mb != parsed_upload:
+            transcription.max_upload_mb = parsed_upload
+            changed = True
+
+    if changed:
+        save_config(config)
+    return settings_payload()

blackcat/webui/transcription_ws.py
@@ -1,0 +1,46 @@
+"""WebUI transcription envelope handling.
+
+The WebSocket channel owns transport and subscription fan-out. This module owns
+the WebUI-specific audio transcription action carried over that socket.
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+from blackcat.audio.transcription import (
+    TranscriptionIngressError,
+    resolve_transcription_config,
+    transcribe_audio_data_url,
+)
+from blackcat.config.loader import load_config
+
+_MAX_REQUEST_ID_LENGTH = 80
+
+
+async def webui_transcription_event(envelope: dict[str, Any]) -> tuple[str, dict[str, Any]]:
+    """Return the WS event name and payload for one WebUI transcription request."""
+    request_id = envelope.get("request_id")
+    valid_request_id = (
+        isinstance(request_id, str)
+        and 0 < len(request_id) <= _MAX_REQUEST_ID_LENGTH
+    )
+
+    def error(detail: str, **extra: Any) -> tuple[str, dict[str, Any]]:
+        payload: dict[str, Any] = {"detail": detail, **extra}
+        if valid_request_id:
+            payload["request_id"] = request_id
+        return "transcription_error", payload
+
+    if not valid_request_id:
+        return error("invalid_request")
+
+    try:
+        text = await transcribe_audio_data_url(
+            envelope.get("data_url"),
+            resolve_transcription_config(load_config()),
+            duration_ms=envelope.get("duration_ms"),
+        )
+    except TranscriptionIngressError as exc:
+        return error(exc.detail, **exc.extra)
+    return "transcription_result", {"request_id": request_id, "text": text}

tests/providers/test_transcription.py
@@ -9,6 +9,8 @@ import httpx
 import pytest
 
 from blackcat.providers.transcription import (
+from blackcat.audio.transcription import resolve_transcription_config
+from blackcat.config.schema import Config
     GroqTranscriptionProvider,
     OpenAITranscriptionProvider,
     _resolve_transcription_url,
@@ -31,6 +33,65 @@ def _raw_response(status: int, content: bytes) -> httpx.Response:
     """Build a Response with a raw, possibly-malformed body (bypasses json= encoding)."""
     request = httpx.Request("POST", "https://example.test/audio/transcriptions")
     return httpx.Response(status_code=status, content=content, request=request)
+
+
+def test_resolver_uses_legacy_channel_provider_when_top_level_is_unset() -> None:
+    config = Config()
+    config.channels.transcription_provider = "openai"
+    config.channels.transcription_language = "en"
+    config.providers.openai.api_key = "sk-test"
+    config.providers.openai.api_base = "https://proxy.example/v1"
+
+    resolved = resolve_transcription_config(config)
+
+    assert resolved.provider == "openai"
+    assert resolved.model == "whisper-1"
+    assert resolved.language == "en"
+    assert resolved.api_key == "sk-test"
+    assert resolved.api_base == "https://proxy.example/v1"
+    assert resolved.configured is True
+
+
+def test_resolver_prefers_top_level_transcription_over_legacy_channels() -> None:
+    config = Config()
+    config.channels.transcription_provider = "openai"
+    config.channels.transcription_language = "en"
+    config.transcription.provider = "groq"
+    config.transcription.model = "whisper-large-v3-turbo"
+    config.transcription.language = "ko"
+    config.providers.groq.api_key = "gsk-test"
+    config.providers.groq.api_base = "https://groq.example/openai/v1"
+
+    resolved = resolve_transcription_config(config)
+
+    assert resolved.provider == "groq"
+    assert resolved.model == "whisper-large-v3-turbo"
+    assert resolved.language == "ko"
+    assert resolved.api_key == "gsk-test"
+    assert resolved.api_base == "https://groq.example/openai/v1"
+
+
+def test_resolved_transcription_repr_hides_api_key() -> None:
+    config = Config()
+    config.providers.groq.api_key = "gsk-secret"
+
+    resolved = resolve_transcription_config(config)
+
+    assert "gsk-secret" not in repr(resolved)
+    assert "api_key" not in repr(resolved)
+
+
+def test_resolver_keeps_enabled_and_limits_on_effective_config() -> None:
+    config = Config()
+    config.transcription.enabled = False
+    config.transcription.max_duration_sec = 45
+    config.transcription.max_upload_mb = 12
+
+    resolved = resolve_transcription_config(config)
+
+    assert resolved.enabled is False
+    assert resolved.max_duration_sec == 45
+    assert resolved.max_upload_mb == 12
 
 
 # ---------------------------------------------------------------------------
@@ -213,6 +274,32 @@ async def test_provider_omits_language_when_unset(
     assert post.await_count == 1
     files = post.await_args_list[0].kwargs["files"]
     assert "language" not in files
+
+
+@pytest.mark.asyncio
+async def test_provider_forwards_custom_model_in_multipart(audio_file: Path) -> None:
+    provider = GroqTranscriptionProvider(api_key="k", model="whisper-large-v3-turbo")
+    post = AsyncMock(return_value=_response(200, {"text": "ok"}))
+    with patch("httpx.AsyncClient.post", post), patch("asyncio.sleep", AsyncMock()):
+        result = await provider.transcribe(audio_file)
+
+    assert result == "ok"
+    files = post.await_args_list[0].kwargs["files"]
+    assert files["model"] == (None, "whisper-large-v3-turbo")
+
+
+@pytest.mark.asyncio
+async def test_provider_forwards_file_mime_type(tmp_path: Path) -> None:
+    audio = tmp_path / "voice.webm"
+    audio.write_bytes(b"audio")
+    provider = GroqTranscriptionProvider(api_key="k")
+    post = AsyncMock(return_value=_response(200, {"text": "ok"}))
+    with patch("httpx.AsyncClient.post", post), patch("asyncio.sleep", AsyncMock()):
+        result = await provider.transcribe(audio)
+
+    assert result == "ok"
+    files = post.await_args_list[0].kwargs["files"]
+    assert files["file"] == ("voice.webm", b"audio", "audio/webm")
 
 
 @pytest.mark.asyncio