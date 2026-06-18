nanoinfraorg/nanoinfra

fix: hide internal cron prompts from webui

nanobot/session/webui_turns.py
@@ -22,6 +22,7 @@ from nanobot.bus.runtime_events import (
     TurnCompleted,
     TurnRunStatusChanged,
 )
+from nanobot.cron.session_turns import CRON_HISTORY_META
 from nanobot.providers.base import LLMProvider
 from nanobot.session.goal_state import goal_state_ws_blob
 from nanobot.session.manager import Session, SessionManager
@@ -67,6 +68,8 @@ def _title_inputs(session: Session) -> tuple[str, str]:
     assistant_text = ""
     for message in session.messages:
         if message.get("_command") is True:
+            continue
+        if message.get(CRON_HISTORY_META) is True:
             continue
         role = message.get("role")
         content = message.get("content")

nanobot/webui/session_list_index.py
@@ -14,6 +14,7 @@ from typing import Any
 
 from loguru import logger
 
+from nanobot.cron.session_turns import CRON_HISTORY_META
 from nanobot.session.manager import (
     _SESSION_LIST_PREVIEW_MAX_CHARS,
     _SESSION_LIST_PREVIEW_MAX_RECORDS,
@@ -142,6 +143,8 @@ def _preview_from_messages(messages: list[dict[str, Any]]) -> str:
             or scanned_chars > _SESSION_LIST_PREVIEW_MAX_CHARS
         ):
             break
+        if item.get(CRON_HISTORY_META) is True:
+            continue
         text = _message_preview_text(item)
         if not text:
             continue
@@ -192,6 +195,8 @@ def _scan_session_row(session_manager: SessionManager, path: Path) -> dict[str, 
                     break
                 item = json.loads(line)
                 if item.get("_type") == "metadata":
+                    continue
+                if item.get(CRON_HISTORY_META) is True:
                     continue
                 text = _message_preview_text(item)
                 if not text:

nanobot/webui/transcript.py
@@ -17,6 +17,7 @@ from urllib.parse import unquote, urlparse
 from loguru import logger
 
 from nanobot.config.paths import get_webui_dir
+from nanobot.cron.session_turns import CRON_HISTORY_META
 from nanobot.session.manager import SessionManager
 from nanobot.webui.metadata import WEBUI_MESSAGE_SOURCE_METADATA_KEY, WEBUI_TURN_METADATA_KEY
 
@@ -853,6 +854,8 @@ def _session_user_event(
     message: dict[str, Any],
 ) -> dict[str, Any] | None:
     if message.get("role") != "user":
+        return None
+    if message.get(CRON_HISTORY_META) is True:
         return None
     content = message.get("content")
     text = content if isinstance(content, str) else ""

HKUDS/nanobot

blackcat/session/webui_turns.py
@@ -22,10 +22,11 @@ from blackcat.bus.runtime_events import (
     TurnCompleted,
     TurnRunStatusChanged,
 )
+from blackcat.cron.session_turns import CRON_HISTORY_META
 from blackcat.providers.base import LLMProvider
 from blackcat.session.goal_state import goal_state_ws_blob
 from blackcat.session.manager import Session, SessionManager
-from blackcat.utils.formatting import strip_think, truncate_text
+from blackcat.utils.helpers import strip_think, truncate_text
 from blackcat.utils.llm_runtime import LLMRuntime
 
 WEBUI_SESSION_METADATA_KEY = "webui"
@@ -67,6 +68,8 @@ def _title_inputs(session: Session) -> tuple[str, str]:
     assistant_text = ""
     for message in session.messages:
         if message.get("_command") is True:
+            continue
+        if message.get(CRON_HISTORY_META) is True:
             continue
         role = message.get("role")
         content = message.get("content")

blackcat/webui/session_list_index.py
@@ -14,6 +14,7 @@ from typing import Any
 
 from loguru import logger
 
+from blackcat.cron.session_turns import CRON_HISTORY_META
 from blackcat.session.manager import (
     _SESSION_LIST_PREVIEW_MAX_CHARS,
     _SESSION_LIST_PREVIEW_MAX_RECORDS,
@@ -142,6 +143,8 @@ def _preview_from_messages(messages: list[dict[str, Any]]) -> str:
             or scanned_chars > _SESSION_LIST_PREVIEW_MAX_CHARS
         ):
             break
+        if item.get(CRON_HISTORY_META) is True:
+            continue
         text = _message_preview_text(item)
         if not text:
             continue
@@ -192,6 +195,8 @@ def _scan_session_row(session_manager: SessionManager, path: Path) -> dict[str, 
                     break
                 item = json.loads(line)
                 if item.get("_type") == "metadata":
+                    continue
+                if item.get(CRON_HISTORY_META) is True:
                     continue
                 text = _message_preview_text(item)
                 if not text:

blackcat/webui/transcript.py
@@ -17,6 +17,7 @@ from urllib.parse import unquote, urlparse
 from loguru import logger
 
 from blackcat.config.paths import get_webui_dir
+from blackcat.cron.session_turns import CRON_HISTORY_META
 from blackcat.session.manager import SessionManager
 from blackcat.webui.metadata import WEBUI_MESSAGE_SOURCE_METADATA_KEY, WEBUI_TURN_METADATA_KEY
 
@@ -853,6 +854,8 @@ def _session_user_event(
     message: dict[str, Any],
 ) -> dict[str, Any] | None:
     if message.get("role") != "user":
+        return None
+    if message.get(CRON_HISTORY_META) is True:
         return None
     content = message.get("content")
     text = content if isinstance(content, str) else ""