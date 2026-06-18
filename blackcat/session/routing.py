nanoinfraorg/nanoinfra

feat(cron): bind scheduled automations to sessions

nanobot/session/routing.py
@@ -1,0 +1,105 @@
+"""Persisted session routing context for proactive turns."""
+
+from __future__ import annotations
+
+from typing import Any, Mapping
+
+from nanobot.bus.events import InboundMessage
+from nanobot.cron.automation import is_automation_turn
+from nanobot.session.manager import Session
+
+SESSION_ROUTING_METADATA_KEY = "_routing_context"
+
+_ROUTING_METADATA_KEYS = {
+    "chat_type",
+    "context_chat_id",
+    "conversation_type",
+    "event_id",
+    "message_thread_id",
+    "msg_type",
+    "parent_channel_id",
+    "parent_id",
+    "platform",
+    "root_id",
+    "thread_id",
+    "thread_reply_to_event_id",
+    "thread_root_event_id",
+}
+_CHANNEL_ROUTING_METADATA_KEYS = {
+    # Feishu needs a message anchor to reply into an existing topic. Other
+    # channels should avoid stale reply anchors for scheduled automation turns.
+    "feishu": {"message_id"},
+}
+_SLACK_ROUTING_KEYS = {"channel_type", "thread_ts"}
+
+
+def _scalar(value: Any) -> str | int | float | bool | None:
+    if value is None or isinstance(value, (str, int, float, bool)):
+        return value
+    return None
+
+
+def _routing_metadata(channel: str, metadata: Mapping[str, Any] | None) -> dict[str, Any]:
+    if not isinstance(metadata, Mapping):
+        return {}
+
+    out: dict[str, Any] = {}
+    keys = _ROUTING_METADATA_KEYS | _CHANNEL_ROUTING_METADATA_KEYS.get(channel, set())
+    for key in keys:
+        if key not in metadata:
+            continue
+        value = _scalar(metadata.get(key))
+        if value is not None:
+            out[key] = value
+
+    slack = metadata.get("slack")
+    if isinstance(slack, Mapping):
+        slack_out = {
+            key: value
+            for key in _SLACK_ROUTING_KEYS
+            if (value := _scalar(slack.get(key))) is not None
+        }
+        if slack_out:
+            out["slack"] = slack_out
+
+    return out
+
+
+def routing_context_for_message(msg: InboundMessage) -> dict[str, Any]:
+    """Return the stable routing context needed to deliver future session turns."""
+    return {
+        "channel": msg.channel,
+        "chat_id": msg.chat_id,
+        "metadata": _routing_metadata(msg.channel, msg.metadata),
+    }
+
+
+def persist_routing_context(session: Session, msg: InboundMessage) -> bool:
+    """Persist the latest non-automation delivery context for a session."""
+    if is_automation_turn(msg.metadata):
+        return False
+    context = routing_context_for_message(msg)
+    if session.metadata.get(SESSION_ROUTING_METADATA_KEY) == context:
+        return False
+    session.metadata[SESSION_ROUTING_METADATA_KEY] = context
+    return True
+
+
+def read_routing_context(metadata: Mapping[str, Any] | None) -> tuple[str, str, dict[str, Any]] | None:
+    """Decode a persisted routing context from session metadata."""
+    if not isinstance(metadata, Mapping):
+        return None
+    raw = metadata.get(SESSION_ROUTING_METADATA_KEY)
+    if not isinstance(raw, Mapping):
+        return None
+
+    channel = raw.get("channel")
+    chat_id = raw.get("chat_id")
+    if not isinstance(channel, str) or not channel:
+        return None
+    if not isinstance(chat_id, str) or not chat_id:
+        return None
+
+    route_meta = raw.get("metadata")
+    metadata_out = dict(route_meta) if isinstance(route_meta, Mapping) else {}
+    return channel, chat_id, metadata_out

nanobot/webui/ws_http.py
@@ -61,7 +61,11 @@ from nanobot.webui.http_utils import (
     safe_host_header as _safe_host_header,
 )
 from nanobot.webui.media_gateway import WebUIMediaGateway
-from nanobot.webui.session_automations import session_automations_payload
+from nanobot.webui.session_automations import (
+    bound_session_automation_jobs,
+    serialize_automation_jobs,
+    session_automations_payload,
+)
 from nanobot.webui.session_list_index import list_webui_sessions
 from nanobot.webui.sidebar_state import (
     read_webui_sidebar_state,
@@ -446,6 +450,20 @@ class GatewayHTTPHandler:
             return _http_error(400, "invalid session key")
         if not _is_websocket_channel_session_key(decoded_key):
             return _http_error(404, "session not found")
+        query = _parse_query(request.path)
+        delete_automations = (_query_first(query, "delete_automations") or "").lower()
+        bound_jobs = bound_session_automation_jobs(self.cron_service, decoded_key)
+        if bound_jobs and delete_automations not in {"1", "true", "yes"}:
+            return _http_json_response(
+                {
+                    "deleted": False,
+                    "blocked_by_automations": True,
+                    "automations": serialize_automation_jobs(bound_jobs),
+                }
+            )
+        if bound_jobs and self.cron_service is not None:
+            for job in bound_jobs:
+                self.cron_service.remove_job(job.id)
         deleted = self.session_manager.delete_session(decoded_key)
         delete_webui_thread(decoded_key)
         return _http_json_response({"deleted": bool(deleted)})

tests/agent/test_loop_save_turn.py
@@ -11,6 +11,7 @@ from nanobot.bus.queue import MessageBus
 from nanobot.providers.base import LLMResponse
 from nanobot.session.goal_state import GOAL_STATE_KEY
 from nanobot.session.manager import Session, SessionManager
+from nanobot.session.routing import SESSION_ROUTING_METADATA_KEY
 from nanobot.session.turn_continuation import (
     INTERNAL_CONTINUATION_META,
     INTERNAL_CONTINUATION_RUN_STARTED_AT_META,
@@ -827,6 +828,12 @@ async def test_process_message_uses_context_chat_id_for_runtime_prompt(tmp_path:
     assert result.chat_id == "thread-777"
     assert loop.context.build_messages.call_args.kwargs["chat_id"] == "parent-456"
     assert loop._run_agent_loop.call_args.kwargs["chat_id"] == "thread-777"
+    session = loop.sessions.get_or_create("discord:parent-456:thread:thread-777")
+    assert session.metadata[SESSION_ROUTING_METADATA_KEY] == {
+        "channel": "discord",
+        "chat_id": "thread-777",
+        "metadata": {"context_chat_id": "parent-456"},
+    }
 
 
 @pytest.mark.asyncio

tests/agent/test_session_manager_history.py
@@ -1,4 +1,5 @@
 from nanobot.session.manager import Session, SessionManager
+from nanobot.session.routing import SESSION_ROUTING_METADATA_KEY
 
 
 def _assert_no_orphans(history: list[dict]) -> None:
@@ -432,6 +433,11 @@ def test_fork_session_before_user_index_copies_only_prefix(tmp_path):
     source.metadata["webui"] = True
     source.metadata["title"] = "Old title"
     source.metadata["goal_state"] = {"status": "active", "objective": "do not inherit"}
+    source.metadata[SESSION_ROUTING_METADATA_KEY] = {
+        "channel": "websocket",
+        "chat_id": "source",
+        "metadata": {},
+    }
     source.add_message("user", "round1")
     source.add_message("assistant", "answer1")
     source.add_message("user", "round2 fork me")
@@ -450,6 +456,7 @@ def test_fork_session_before_user_index_copies_only_prefix(tmp_path):
     assert forked.metadata["webui"] is True
     assert "title" not in forked.metadata
     assert "goal_state" not in forked.metadata
+    assert SESSION_ROUTING_METADATA_KEY not in forked.metadata
     saved = manager.read_session_file("websocket:fork")
     assert [m["content"] for m in saved["messages"]] == ["round1", "answer1"]
 

HKUDS/nanobot

blackcat/session/routing.py
@@ -1,0 +1,105 @@
+"""Persisted session routing context for proactive turns."""
+
+from __future__ import annotations
+
+from typing import Any, Mapping
+
+from blackcat.bus.events import InboundMessage
+from blackcat.cron.automation import is_automation_turn
+from blackcat.session.manager import Session
+
+SESSION_ROUTING_METADATA_KEY = "_routing_context"
+
+_ROUTING_METADATA_KEYS = {
+    "chat_type",
+    "context_chat_id",
+    "conversation_type",
+    "event_id",
+    "message_thread_id",
+    "msg_type",
+    "parent_channel_id",
+    "parent_id",
+    "platform",
+    "root_id",
+    "thread_id",
+    "thread_reply_to_event_id",
+    "thread_root_event_id",
+}
+_CHANNEL_ROUTING_METADATA_KEYS = {
+    # Feishu needs a message anchor to reply into an existing topic. Other
+    # channels should avoid stale reply anchors for scheduled automation turns.
+    "feishu": {"message_id"},
+}
+_SLACK_ROUTING_KEYS = {"channel_type", "thread_ts"}
+
+
+def _scalar(value: Any) -> str | int | float | bool | None:
+    if value is None or isinstance(value, (str, int, float, bool)):
+        return value
+    return None
+
+
+def _routing_metadata(channel: str, metadata: Mapping[str, Any] | None) -> dict[str, Any]:
+    if not isinstance(metadata, Mapping):
+        return {}
+
+    out: dict[str, Any] = {}
+    keys = _ROUTING_METADATA_KEYS | _CHANNEL_ROUTING_METADATA_KEYS.get(channel, set())
+    for key in keys:
+        if key not in metadata:
+            continue
+        value = _scalar(metadata.get(key))
+        if value is not None:
+            out[key] = value
+
+    slack = metadata.get("slack")
+    if isinstance(slack, Mapping):
+        slack_out = {
+            key: value
+            for key in _SLACK_ROUTING_KEYS
+            if (value := _scalar(slack.get(key))) is not None
+        }
+        if slack_out:
+            out["slack"] = slack_out
+
+    return out
+
+
+def routing_context_for_message(msg: InboundMessage) -> dict[str, Any]:
+    """Return the stable routing context needed to deliver future session turns."""
+    return {
+        "channel": msg.channel,
+        "chat_id": msg.chat_id,
+        "metadata": _routing_metadata(msg.channel, msg.metadata),
+    }
+
+
+def persist_routing_context(session: Session, msg: InboundMessage) -> bool:
+    """Persist the latest non-automation delivery context for a session."""
+    if is_automation_turn(msg.metadata):
+        return False
+    context = routing_context_for_message(msg)
+    if session.metadata.get(SESSION_ROUTING_METADATA_KEY) == context:
+        return False
+    session.metadata[SESSION_ROUTING_METADATA_KEY] = context
+    return True
+
+
+def read_routing_context(metadata: Mapping[str, Any] | None) -> tuple[str, str, dict[str, Any]] | None:
+    """Decode a persisted routing context from session metadata."""
+    if not isinstance(metadata, Mapping):
+        return None
+    raw = metadata.get(SESSION_ROUTING_METADATA_KEY)
+    if not isinstance(raw, Mapping):
+        return None
+
+    channel = raw.get("channel")
+    chat_id = raw.get("chat_id")
+    if not isinstance(channel, str) or not channel:
+        return None
+    if not isinstance(chat_id, str) or not chat_id:
+        return None
+
+    route_meta = raw.get("metadata")
+    metadata_out = dict(route_meta) if isinstance(route_meta, Mapping) else {}
+    return channel, chat_id, metadata_out

blackcat/webui/ws_http.py
@@ -61,7 +61,11 @@ from blackcat.webui.http_utils import (
     safe_host_header as _safe_host_header,
 )
 from blackcat.webui.media_gateway import WebUIMediaGateway
-from blackcat.webui.session_automations import session_automations_payload
+from blackcat.webui.session_automations import (
+    bound_session_automation_jobs,
+    serialize_automation_jobs,
+    session_automations_payload,
+)
 from blackcat.webui.session_list_index import list_webui_sessions
 from blackcat.webui.sidebar_state import (
     read_webui_sidebar_state,
@@ -446,6 +450,20 @@ class GatewayHTTPHandler:
             return _http_error(400, "invalid session key")
         if not _is_websocket_channel_session_key(decoded_key):
             return _http_error(404, "session not found")
+        query = _parse_query(request.path)
+        delete_automations = (_query_first(query, "delete_automations") or "").lower()
+        bound_jobs = bound_session_automation_jobs(self.cron_service, decoded_key)
+        if bound_jobs and delete_automations not in {"1", "true", "yes"}:
+            return _http_json_response(
+                {
+                    "deleted": False,
+                    "blocked_by_automations": True,
+                    "automations": serialize_automation_jobs(bound_jobs),
+                }
+            )
+        if bound_jobs and self.cron_service is not None:
+            for job in bound_jobs:
+                self.cron_service.remove_job(job.id)
         deleted = self.session_manager.delete_session(decoded_key)
         delete_webui_thread(decoded_key)
         return _http_json_response({"deleted": bool(deleted)})

tests/agent/test_loop_save_turn.py
@@ -11,6 +11,7 @@ from blackcat.bus.queue import MessageBus
 from blackcat.providers.base import LLMResponse
 from blackcat.session.goal_state import GOAL_STATE_KEY
 from blackcat.session.manager import Session, SessionManager
+from blackcat.session.routing import SESSION_ROUTING_METADATA_KEY
 from blackcat.session.turn_continuation import (
     INTERNAL_CONTINUATION_META,
     INTERNAL_CONTINUATION_RUN_STARTED_AT_META,
@@ -794,7 +795,7 @@ async def test_websocket_internal_continuation_keeps_single_visible_run(
 async def test_process_message_uses_context_chat_id_for_runtime_prompt(tmp_path: Path) -> None:
     loop = _make_full_loop(tmp_path)
     loop.consolidator.maybe_consolidate_by_tokens = AsyncMock(return_value=False)  # type: ignore[method-assign]
-    loop.context.build_messages = AsyncMock(  # type: ignore[method-assign]
+    loop.context.build_messages = MagicMock(  # type: ignore[method-assign]
         return_value=[
             {"role": "system", "content": "system"},
             {"role": "user", "content": "runtime + hello"},
@@ -827,6 +828,12 @@ async def test_process_message_uses_context_chat_id_for_runtime_prompt(tmp_path:
     assert result.chat_id == "thread-777"
     assert loop.context.build_messages.call_args.kwargs["chat_id"] == "parent-456"
     assert loop._run_agent_loop.call_args.kwargs["chat_id"] == "thread-777"
+    session = loop.sessions.get_or_create("discord:parent-456:thread:thread-777")
+    assert session.metadata[SESSION_ROUTING_METADATA_KEY] == {
+        "channel": "discord",
+        "chat_id": "thread-777",
+        "metadata": {"context_chat_id": "parent-456"},
+    }
 
 
 @pytest.mark.asyncio
@@ -845,7 +852,7 @@ async def test_process_message_uses_explicit_session_metadata_for_goal_context(
     system_session.metadata = {}
     loop.sessions.save(system_session)
 
-    loop.context.build_messages = AsyncMock(  # type: ignore[method-assign]
+    loop.context.build_messages = MagicMock(  # type: ignore[method-assign]
         return_value=[
             {"role": "system", "content": "system"},
             {"role": "user", "content": "runtime + system"},
@@ -1150,7 +1157,7 @@ async def test_multiple_subagent_followups_all_persist_as_standalone_history(tmp
     ]
 
 
-async def test_prompt_merge_does_not_replace_standalone_subagent_history_entry(tmp_path: Path) -> None:
+def test_prompt_merge_does_not_replace_standalone_subagent_history_entry(tmp_path: Path) -> None:
     loop = _mk_loop()
     session = Session(key="cli:merge")
     session.add_message("assistant", "previous assistant")
@@ -1169,7 +1176,7 @@ async def test_prompt_merge_does_not_replace_standalone_subagent_history_entry(t
     assert inserted is True
 
     builder = ContextBuilder(tmp_path)
-    projected = await builder.build_messages(
+    projected = builder.build_messages(
         history=session.get_history(max_messages=0),
         current_message="",
         current_role="assistant",

tests/agent/test_session_manager_history.py
@@ -1,4 +1,5 @@
 from blackcat.session.manager import Session, SessionManager
+from blackcat.session.routing import SESSION_ROUTING_METADATA_KEY
 
 
 def _assert_no_orphans(history: list[dict]) -> None:
@@ -432,6 +433,11 @@ def test_fork_session_before_user_index_copies_only_prefix(tmp_path):
     source.metadata["webui"] = True
     source.metadata["title"] = "Old title"
     source.metadata["goal_state"] = {"status": "active", "objective": "do not inherit"}
+    source.metadata[SESSION_ROUTING_METADATA_KEY] = {
+        "channel": "websocket",
+        "chat_id": "source",
+        "metadata": {},
+    }
     source.add_message("user", "round1")
     source.add_message("assistant", "answer1")
     source.add_message("user", "round2 fork me")
@@ -450,6 +456,7 @@ def test_fork_session_before_user_index_copies_only_prefix(tmp_path):
     assert forked.metadata["webui"] is True
     assert "title" not in forked.metadata
     assert "goal_state" not in forked.metadata
+    assert SESSION_ROUTING_METADATA_KEY not in forked.metadata
     saved = manager.read_session_file("websocket:fork")
     assert [m["content"] for m in saved["messages"]] == ["round1", "answer1"]
 