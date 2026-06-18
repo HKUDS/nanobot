nanoinfraorg/nanoinfra

refactor: centralize cron session metadata keys

nanobot/cli/commands.py
@@ -64,6 +64,10 @@ from nanobot.utils.restart import (  # noqa: E402
     format_restart_completed_message,
     should_show_cli_restart_notice,
 )
+from nanobot.webui.metadata import (  # noqa: E402
+    WEBUI_MESSAGE_SOURCE_METADATA_KEY,
+    WEBUI_TURN_METADATA_KEY,
+)
 
 
 def _sanitize_surrogates(text: str) -> str:
@@ -89,8 +93,6 @@ class SafeFileHistory(FileHistory):
         super().store_string(_sanitize_surrogates(string))
 
 
-_WEBUI_TURN_META_KEY = "webui_turn_id"
-_WEBUI_MESSAGE_SOURCE_META_KEY = "_webui_message_source"
 _PROACTIVE_WEBUI_METADATA: ContextVar[dict[str, Any] | None] = ContextVar(
     "proactive_webui_metadata",
     default=None,
@@ -106,13 +108,13 @@ def _proactive_delivery_metadata(
 ) -> dict[str, Any]:
     """Return channel metadata for a fresh proactive delivery turn."""
     out = dict(metadata or {})
-    out.pop(_WEBUI_TURN_META_KEY, None)
+    out.pop(WEBUI_TURN_METADATA_KEY, None)
     if channel == "websocket":
-        out[_WEBUI_TURN_META_KEY] = f"{turn_seed}:{uuid.uuid4().hex}"
+        out[WEBUI_TURN_METADATA_KEY] = f"{turn_seed}:{uuid.uuid4().hex}"
         source: dict[str, str] = {"kind": "cron"}
         if source_label:
             source["label"] = source_label
-        out[_WEBUI_MESSAGE_SOURCE_META_KEY] = source
+        out[WEBUI_MESSAGE_SOURCE_METADATA_KEY] = source
     return out
 
 app = typer.Typer(
@@ -1034,14 +1036,14 @@ def _run_gateway(
         schedule_background=lambda coro: agent._schedule_background(coro),
     ).subscribe(runtime_events)
 
-    from nanobot.agent.loop import UNIFIED_SESSION_KEY
     from nanobot.bus.events import InboundMessage, OutboundMessage
+    from nanobot.session.keys import session_key_for_channel
 
     def _channel_session_key(channel: str, chat_id: str) -> str:
-        return (
-            UNIFIED_SESSION_KEY
-            if config.agents.defaults.unified_session
-            else f"{channel}:{chat_id}"
+        return session_key_for_channel(
+            channel,
+            chat_id,
+            unified_session=config.agents.defaults.unified_session,
         )
 
     def _session_metadata(session_key: str) -> dict[str, Any]:
@@ -1125,17 +1127,20 @@ def _run_gateway(
             ),
         }
         metadata[AUTOMATION_DEFER_UNTIL_IDLE_META] = True
+        run_record_base: dict[str, Any] = {
+            "job_id": job.id,
+            "job_name": job.name,
+            "session_key": session_key,
+            "prompt_ref": prompt_ref,
+            "prompt_vars": {"message": job.payload.message},
+            "rendered_prompt": prompt,
+        }
 
         cron.write_run_record(
             run_id,
             {
-                "job_id": job.id,
-                "job_name": job.name,
-                "session_key": session_key,
+                **run_record_base,
                 "status": "queued",
-                "prompt_ref": prompt_ref,
-                "prompt_vars": {"message": job.payload.message},
-                "rendered_prompt": prompt,
             },
         )
 
@@ -1159,14 +1164,9 @@ def _run_gateway(
             cron.write_run_record(
                 run_id,
                 {
-                    "job_id": job.id,
-                    "job_name": job.name,
-                    "session_key": session_key,
+                    **run_record_base,
                     "status": "error",
                     "error": error_text,
-                    "prompt_ref": prompt_ref,
-                    "prompt_vars": {"message": job.payload.message},
-                    "rendered_prompt": prompt,
                 },
             )
             raise
@@ -1178,13 +1178,8 @@ def _run_gateway(
         cron.write_run_record(
             run_id,
             {
-                "job_id": job.id,
-                "job_name": job.name,
-                "session_key": session_key,
+                **run_record_base,
                 "status": "ok",
-                "prompt_ref": prompt_ref,
-                "prompt_vars": {"message": job.payload.message},
-                "rendered_prompt": prompt,
                 "response": response,
             },
         )

nanobot/session/routing.py
@@ -7,8 +7,7 @@ from typing import Any, Mapping
 from nanobot.bus.events import InboundMessage
 from nanobot.cron.automation import is_automation_turn
 from nanobot.session.manager import Session
-
-SESSION_ROUTING_METADATA_KEY = "_routing_context"
+from nanobot.session.metadata import SESSION_ROUTING_METADATA_KEY
 
 _ROUTING_METADATA_KEYS = {
     "chat_type",

nanobot/webui/transcript.py
@@ -18,6 +18,7 @@ from loguru import logger
 
 from nanobot.config.paths import get_webui_dir
 from nanobot.session.manager import SessionManager
+from nanobot.webui.metadata import WEBUI_MESSAGE_SOURCE_METADATA_KEY, WEBUI_TURN_METADATA_KEY
 
 WEBUI_TRANSCRIPT_SCHEMA_VERSION = 3
 WEBUI_FORK_MARKER_EVENT = "fork_marker"
@@ -29,8 +30,6 @@ _TRANSCRIPT_SEGMENT_RE = re.compile(r"^\d{6}\.jsonl$")
 _DEFAULT_TRANSCRIPT_PAGE_LIMIT = 160
 _MAX_TRANSCRIPT_PAGE_LIMIT = 1000
 _WEBUI_TURN_ID_RE = re.compile(r"^[A-Za-z0-9._:-]{1,128}$")
-WEBUI_TURN_METADATA_KEY = "webui_turn_id"
-WEBUI_MESSAGE_SOURCE_METADATA_KEY = "_webui_message_source"
 _MARKDOWN_LOCAL_IMAGE_RE = re.compile(
     r"!\[([^\]]*)\]\((<[^>]+>|[^)\s]+)(\s+(?:\"[^\"]*\"|'[^']*'))?\)"
 )

tests/agent/test_loop_save_turn.py
@@ -8,6 +8,7 @@ from nanobot.agent.context import ContextBuilder
 from nanobot.agent.loop import AgentLoop
 from nanobot.bus.events import InboundMessage
 from nanobot.bus.queue import MessageBus
+from nanobot.cron.automation import AUTOMATION_HISTORY_META, AUTOMATION_TRIGGER_META
 from nanobot.providers.base import LLMResponse
 from nanobot.session.goal_state import GOAL_STATE_KEY
 from nanobot.session.manager import Session, SessionManager
@@ -63,6 +64,41 @@ def test_agent_loop_llm_runtime_reflects_current_provider_and_model(tmp_path: Pa
 
     assert runtime.provider is next_provider
     assert runtime.model == "next-model"
+
+
+def test_persist_automation_turn_uses_distinct_history_marker(tmp_path: Path) -> None:
+    loop = _make_full_loop(tmp_path)
+    session = loop.sessions.get_or_create("websocket:auto")
+    prompt_ref = {"id": "cron.agent_turn.reminder", "version": 1, "sha256": "abc"}
+
+    persisted = loop._persist_user_message_early(
+        InboundMessage(
+            channel="websocket",
+            sender_id="cron",
+            chat_id="auto",
+            content="Automation: internal prompt",
+            metadata={
+                AUTOMATION_TRIGGER_META: {
+                    "job_id": "job-1",
+                    "job_name": "Daily check",
+                    "run_id": "job-1:1",
+                    "prompt_ref": prompt_ref,
+                    "persist_content": "Scheduled automation triggered: Daily check",
+                }
+            },
+        ),
+        session,
+    )
+
+    assert persisted is True
+    message = session.messages[-1]
+    assert message["content"] == "Scheduled automation triggered: Daily check"
+    assert message[AUTOMATION_HISTORY_META] is True
+    assert AUTOMATION_TRIGGER_META not in message
+    assert message["automation_id"] == "job-1"
+    assert message["automation_name"] == "Daily check"
+    assert message["automation_run_id"] == "job-1:1"
+    assert message["automation_prompt_ref"] == prompt_ref
 
 
 def test_clean_generated_title_strips_reasoning_tags() -> None:

tests/channels/test_websocket_http_routes.py
@@ -11,10 +11,10 @@ from urllib.parse import urlencode
 import httpx
 import pytest
 
-from nanobot.agent.loop import UNIFIED_SESSION_KEY
 from nanobot.channels.websocket import WebSocketChannel, WebSocketConfig
 from nanobot.cron.service import CronService
 from nanobot.cron.types import CronJob, CronPayload, CronSchedule
+from nanobot.session.keys import UNIFIED_SESSION_KEY
 from nanobot.session.manager import Session, SessionManager
 from nanobot.webui.gateway_services import GatewayServices, build_gateway_services
 

tests/test_tool_contextvars.py
@@ -10,6 +10,7 @@ from nanobot.agent.tools.cron import CronTool
 from nanobot.agent.tools.message import MessageTool
 from nanobot.agent.tools.spawn import SpawnTool
 from nanobot.cron.service import CronService
+from nanobot.session.keys import UNIFIED_SESSION_KEY
 
 
 @pytest.mark.asyncio
@@ -262,7 +263,7 @@ async def test_webui_cron_tool_uses_unified_session_when_enabled(tmp_path) -> No
         "websocket",
         "chat-123",
         metadata={"webui": True},
-        session_key="unified:default",
+        session_key=UNIFIED_SESSION_KEY,
     )
 
     result = await tool.execute(action="add", message="standup", every_seconds=300)
@@ -270,7 +271,7 @@ async def test_webui_cron_tool_uses_unified_session_when_enabled(tmp_path) -> No
 
     jobs = tool._cron.list_jobs()
     assert len(jobs) == 1
-    assert jobs[0].payload.session_key == "unified:default"
+    assert jobs[0].payload.session_key == UNIFIED_SESSION_KEY
 
 
 @pytest.mark.asyncio

HKUDS/nanobot

blackcat/cli/commands.py
@@ -64,6 +64,10 @@ from blackcat.utils.restart import (  # noqa: E402
     format_restart_completed_message,
     should_show_cli_restart_notice,
 )
+from blackcat.webui.metadata import (  # noqa: E402
+    WEBUI_MESSAGE_SOURCE_METADATA_KEY,
+    WEBUI_TURN_METADATA_KEY,
+)
 
 
 def _sanitize_surrogates(text: str) -> str:
@@ -89,8 +93,6 @@ class SafeFileHistory(FileHistory):
         super().store_string(_sanitize_surrogates(string))
 
 
-_WEBUI_TURN_META_KEY = "webui_turn_id"
-_WEBUI_MESSAGE_SOURCE_META_KEY = "_webui_message_source"
 _PROACTIVE_WEBUI_METADATA: ContextVar[dict[str, Any] | None] = ContextVar(
     "proactive_webui_metadata",
     default=None,
@@ -106,13 +108,13 @@ def _proactive_delivery_metadata(
 ) -> dict[str, Any]:
     """Return channel metadata for a fresh proactive delivery turn."""
     out = dict(metadata or {})
-    out.pop(_WEBUI_TURN_META_KEY, None)
+    out.pop(WEBUI_TURN_METADATA_KEY, None)
     if channel == "websocket":
-        out[_WEBUI_TURN_META_KEY] = f"{turn_seed}:{uuid.uuid4().hex}"
+        out[WEBUI_TURN_METADATA_KEY] = f"{turn_seed}:{uuid.uuid4().hex}"
         source: dict[str, str] = {"kind": "cron"}
         if source_label:
             source["label"] = source_label
-        out[_WEBUI_MESSAGE_SOURCE_META_KEY] = source
+        out[WEBUI_MESSAGE_SOURCE_METADATA_KEY] = source
     return out
 
 app = typer.Typer(
@@ -1034,14 +1036,14 @@ def _run_gateway(
         schedule_background=lambda coro: agent._schedule_background(coro),
     ).subscribe(runtime_events)
 
-    from blackcat.agent.loop import UNIFIED_SESSION_KEY
     from blackcat.bus.events import InboundMessage, OutboundMessage
+    from blackcat.session.keys import session_key_for_channel
 
     def _channel_session_key(channel: str, chat_id: str) -> str:
-        return (
-            UNIFIED_SESSION_KEY
-            if config.agents.defaults.unified_session
-            else f"{channel}:{chat_id}"
+        return session_key_for_channel(
+            channel,
+            chat_id,
+            unified_session=config.agents.defaults.unified_session,
         )
 
     def _session_metadata(session_key: str) -> dict[str, Any]:
@@ -1125,17 +1127,20 @@ def _run_gateway(
             ),
         }
         metadata[AUTOMATION_DEFER_UNTIL_IDLE_META] = True
+        run_record_base: dict[str, Any] = {
+            "job_id": job.id,
+            "job_name": job.name,
+            "session_key": session_key,
+            "prompt_ref": prompt_ref,
+            "prompt_vars": {"message": job.payload.message},
+            "rendered_prompt": prompt,
+        }
 
         cron.write_run_record(
             run_id,
             {
-                "job_id": job.id,
-                "job_name": job.name,
-                "session_key": session_key,
+                **run_record_base,
                 "status": "queued",
-                "prompt_ref": prompt_ref,
-                "prompt_vars": {"message": job.payload.message},
-                "rendered_prompt": prompt,
             },
         )
 
@@ -1159,14 +1164,9 @@ def _run_gateway(
             cron.write_run_record(
                 run_id,
                 {
-                    "job_id": job.id,
-                    "job_name": job.name,
-                    "session_key": session_key,
+                    **run_record_base,
                     "status": "error",
                     "error": error_text,
-                    "prompt_ref": prompt_ref,
-                    "prompt_vars": {"message": job.payload.message},
-                    "rendered_prompt": prompt,
                 },
             )
             raise
@@ -1178,13 +1178,8 @@ def _run_gateway(
         cron.write_run_record(
             run_id,
             {
-                "job_id": job.id,
-                "job_name": job.name,
-                "session_key": session_key,
+                **run_record_base,
                 "status": "ok",
-                "prompt_ref": prompt_ref,
-                "prompt_vars": {"message": job.payload.message},
-                "rendered_prompt": prompt,
                 "response": response,
             },
         )

blackcat/session/routing.py
@@ -7,8 +7,7 @@ from typing import Any, Mapping
 from blackcat.bus.events import InboundMessage
 from blackcat.cron.automation import is_automation_turn
 from blackcat.session.manager import Session
-
-SESSION_ROUTING_METADATA_KEY = "_routing_context"
+from blackcat.session.metadata import SESSION_ROUTING_METADATA_KEY
 
 _ROUTING_METADATA_KEYS = {
     "chat_type",

blackcat/webui/transcript.py
@@ -18,6 +18,7 @@ from loguru import logger
 
 from blackcat.config.paths import get_webui_dir
 from blackcat.session.manager import SessionManager
+from blackcat.webui.metadata import WEBUI_MESSAGE_SOURCE_METADATA_KEY, WEBUI_TURN_METADATA_KEY
 
 WEBUI_TRANSCRIPT_SCHEMA_VERSION = 3
 WEBUI_FORK_MARKER_EVENT = "fork_marker"
@@ -29,8 +30,6 @@ _TRANSCRIPT_SEGMENT_RE = re.compile(r"^\d{6}\.jsonl$")
 _DEFAULT_TRANSCRIPT_PAGE_LIMIT = 160
 _MAX_TRANSCRIPT_PAGE_LIMIT = 1000
 _WEBUI_TURN_ID_RE = re.compile(r"^[A-Za-z0-9._:-]{1,128}$")
-WEBUI_TURN_METADATA_KEY = "webui_turn_id"
-WEBUI_MESSAGE_SOURCE_METADATA_KEY = "_webui_message_source"
 _MARKDOWN_LOCAL_IMAGE_RE = re.compile(
     r"!\[([^\]]*)\]\((<[^>]+>|[^)\s]+)(\s+(?:\"[^\"]*\"|'[^']*'))?\)"
 )
@@ -1204,7 +1203,7 @@ def replay_transcript_to_ui_messages(
 ) -> list[dict[str, Any]]:
     """Fold JSONL records into ``UIMessage``-shaped dicts for the WebUI.
 
-    Mirrors the core fold in ``useBlackcatStream.ts`` (delta, reasoning,
+    Mirrors the core fold in ``useNanobotStream.ts`` (delta, reasoning,
     message+kind, turn_end). ``augment_user_media`` maps persisted filesystem
     paths to ``{url, name?}`` / attachment dicts the client expects. Assistant
     media gets a separate hook so replay can re-sign outbound attachments after

tests/agent/test_loop_save_turn.py
@@ -8,6 +8,7 @@ from blackcat.agent.context import ContextBuilder
 from blackcat.agent.loop import AgentLoop
 from blackcat.bus.events import InboundMessage
 from blackcat.bus.queue import MessageBus
+from blackcat.cron.automation import AUTOMATION_HISTORY_META, AUTOMATION_TRIGGER_META
 from blackcat.providers.base import LLMResponse
 from blackcat.session.goal_state import GOAL_STATE_KEY
 from blackcat.session.manager import Session, SessionManager
@@ -63,6 +64,41 @@ def test_agent_loop_llm_runtime_reflects_current_provider_and_model(tmp_path: Pa
 
     assert runtime.provider is next_provider
     assert runtime.model == "next-model"
+
+
+def test_persist_automation_turn_uses_distinct_history_marker(tmp_path: Path) -> None:
+    loop = _make_full_loop(tmp_path)
+    session = loop.sessions.get_or_create("websocket:auto")
+    prompt_ref = {"id": "cron.agent_turn.reminder", "version": 1, "sha256": "abc"}
+
+    persisted = loop._persist_user_message_early(
+        InboundMessage(
+            channel="websocket",
+            sender_id="cron",
+            chat_id="auto",
+            content="Automation: internal prompt",
+            metadata={
+                AUTOMATION_TRIGGER_META: {
+                    "job_id": "job-1",
+                    "job_name": "Daily check",
+                    "run_id": "job-1:1",
+                    "prompt_ref": prompt_ref,
+                    "persist_content": "Scheduled automation triggered: Daily check",
+                }
+            },
+        ),
+        session,
+    )
+
+    assert persisted is True
+    message = session.messages[-1]
+    assert message["content"] == "Scheduled automation triggered: Daily check"
+    assert message[AUTOMATION_HISTORY_META] is True
+    assert AUTOMATION_TRIGGER_META not in message
+    assert message["automation_id"] == "job-1"
+    assert message["automation_name"] == "Daily check"
+    assert message["automation_run_id"] == "job-1:1"
+    assert message["automation_prompt_ref"] == prompt_ref
 
 
 def test_clean_generated_title_strips_reasoning_tags() -> None:

tests/channels/test_websocket_http_routes.py
@@ -11,10 +11,10 @@ from urllib.parse import urlencode
 import httpx
 import pytest
 
-from blackcat.agent.loop import UNIFIED_SESSION_KEY
 from blackcat.channels.websocket import WebSocketChannel, WebSocketConfig
 from blackcat.cron.service import CronService
 from blackcat.cron.types import CronJob, CronPayload, CronSchedule
+from blackcat.session.keys import UNIFIED_SESSION_KEY
 from blackcat.session.manager import Session, SessionManager
 from blackcat.webui.gateway_services import GatewayServices, build_gateway_services
 

tests/test_tool_contextvars.py
@@ -10,6 +10,7 @@ from blackcat.agent.tools.cron import CronTool
 from blackcat.agent.tools.message import MessageTool
 from blackcat.agent.tools.spawn import SpawnTool
 from blackcat.cron.service import CronService
+from blackcat.session.keys import UNIFIED_SESSION_KEY
 
 
 @pytest.mark.asyncio
@@ -262,7 +263,7 @@ async def test_webui_cron_tool_uses_unified_session_when_enabled(tmp_path) -> No
         "websocket",
         "chat-123",
         metadata={"webui": True},
-        session_key="unified:default",
+        session_key=UNIFIED_SESSION_KEY,
     )
 
     result = await tool.execute(action="add", message="standup", every_seconds=300)
@@ -270,7 +271,7 @@ async def test_webui_cron_tool_uses_unified_session_when_enabled(tmp_path) -> No
 
     jobs = tool._cron.list_jobs()
     assert len(jobs) == 1
-    assert jobs[0].payload.session_key == "unified:default"
+    assert jobs[0].payload.session_key == UNIFIED_SESSION_KEY
 
 
 @pytest.mark.asyncio