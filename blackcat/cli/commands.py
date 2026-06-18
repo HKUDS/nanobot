nanoinfraorg/nanoinfra

fix: harden cron session automation flows

nanobot/cli/commands.py
@@ -947,7 +947,7 @@ def _run_gateway(
     from nanobot.bus.runtime_events import RuntimeEventBus
     from nanobot.channels.manager import ChannelManager
     from nanobot.cron.bound_runner import run_bound_cron_job
-    from nanobot.cron.service import CronService
+    from nanobot.cron.service import CronJobSkippedError, CronService
     from nanobot.cron.session_turns import is_bound_cron_job
     from nanobot.cron.types import CronJob
     from nanobot.providers.factory import build_provider_snapshot, load_provider_snapshot
@@ -1167,12 +1167,14 @@ def _run_gateway(
         if is_bound_cron_job(job):
             return await run_bound_cron_job(job, agent=agent, cron=cron)
 
+        reason = "unbound agent cron job must be recreated from a chat session"
         logger.warning(
-            "Cron: skipped unbound agent job '{}' ({}); recreate it from a chat session",
+            "Cron: skipped unbound agent job '{}' ({}): {}",
             job.name,
             job.id,
+            reason,
         )
-        return None
+        raise CronJobSkippedError(reason)
 
     cron.on_job = on_cron_job
 
@@ -1191,6 +1193,7 @@ def _run_gateway(
         session_manager=session_manager,
         cron_service=cron,
         webui_runtime_model_name=_webui_runtime_model_name,
+        webui_cron_pending_job_ids=getattr(agent, "pending_cron_job_ids_for_session", None),
         webui_static_dist=webui_static_dist,
         webui_runtime_surface=webui_runtime_surface,
         webui_runtime_capabilities=webui_runtime_capabilities,

tests/cli/test_commands.py
@@ -11,6 +11,7 @@ from typer.testing import CliRunner
 from nanobot.bus.events import InboundMessage, OutboundMessage
 from nanobot.cli.commands import app
 from nanobot.config.schema import Config
+from nanobot.cron.service import CronJobSkippedError
 from nanobot.cron.session_turns import CRON_DEFER_UNTIL_IDLE_META, CRON_TRIGGER_META
 from nanobot.cron.types import CronJob, CronPayload
 from nanobot.cron.webui_metadata import cron_proactive_delivery_metadata
@@ -1305,9 +1306,9 @@ def test_gateway_unbound_agent_cron_is_skipped(
         ),
     )
 
-    response = asyncio.run(cron.on_job(job))
+    with pytest.raises(CronJobSkippedError, match="unbound agent cron job"):
+        asyncio.run(cron.on_job(job))
 
-    assert response is None
     bus.publish_outbound.assert_not_awaited()
 
 

tests/cron/test_cron_service.py
@@ -4,7 +4,7 @@ import time
 
 import pytest
 
-from nanobot.cron.service import CronService
+from nanobot.cron.service import CronJobSkippedError, CronService
 from nanobot.cron.types import CronJob, CronPayload, CronSchedule
 
 
@@ -294,6 +294,30 @@ async def test_run_history_records_errors(tmp_path) -> None:
     assert len(loaded.state.run_history) == 1
     assert loaded.state.run_history[0].status == "error"
     assert loaded.state.run_history[0].error == "boom"
+
+
+@pytest.mark.asyncio
+async def test_run_history_records_skipped_jobs(tmp_path) -> None:
+    store_path = tmp_path / "cron" / "jobs.json"
+
+    async def skip(_):
+        raise CronJobSkippedError("missing session binding")
+
+    service = CronService(store_path, on_job=skip)
+    job = service.add_job(
+        name="skip",
+        schedule=CronSchedule(kind="every", every_ms=60_000),
+        message="hello",
+    )
+    await service.run_job(job.id)
+
+    loaded = service.get_job(job.id)
+    assert loaded is not None
+    assert loaded.state.last_status == "skipped"
+    assert loaded.state.last_error == "missing session binding"
+    assert len(loaded.state.run_history) == 1
+    assert loaded.state.run_history[0].status == "skipped"
+    assert loaded.state.run_history[0].error == "missing session binding"
 
 
 @pytest.mark.asyncio

HKUDS/nanobot

blackcat/cli/commands.py
@@ -947,7 +947,7 @@ def _run_gateway(
     from blackcat.bus.runtime_events import RuntimeEventBus
     from blackcat.channels.manager import ChannelManager
     from blackcat.cron.bound_runner import run_bound_cron_job
-    from blackcat.cron.service import CronService
+    from blackcat.cron.service import CronJobSkippedError, CronService
     from blackcat.cron.session_turns import is_bound_cron_job
     from blackcat.cron.types import CronJob
     from blackcat.providers.factory import build_provider_snapshot, load_provider_snapshot
@@ -1167,12 +1167,14 @@ def _run_gateway(
         if is_bound_cron_job(job):
             return await run_bound_cron_job(job, agent=agent, cron=cron)
 
+        reason = "unbound agent cron job must be recreated from a chat session"
         logger.warning(
-            "Cron: skipped unbound agent job '{}' ({}); recreate it from a chat session",
+            "Cron: skipped unbound agent job '{}' ({}): {}",
             job.name,
             job.id,
+            reason,
         )
-        return None
+        raise CronJobSkippedError(reason)
 
     cron.on_job = on_cron_job
 
@@ -1191,6 +1193,7 @@ def _run_gateway(
         session_manager=session_manager,
         cron_service=cron,
         webui_runtime_model_name=_webui_runtime_model_name,
+        webui_cron_pending_job_ids=getattr(agent, "pending_cron_job_ids_for_session", None),
         webui_static_dist=webui_static_dist,
         webui_runtime_surface=webui_runtime_surface,
         webui_runtime_capabilities=webui_runtime_capabilities,

tests/cli/test_commands.py
@@ -11,6 +11,7 @@ from typer.testing import CliRunner
 from blackcat.bus.events import InboundMessage, OutboundMessage
 from blackcat.cli.commands import app
 from blackcat.config.schema import Config
+from blackcat.cron.service import CronJobSkippedError
 from blackcat.cron.session_turns import CRON_DEFER_UNTIL_IDLE_META, CRON_TRIGGER_META
 from blackcat.cron.types import CronJob, CronPayload
 from blackcat.cron.webui_metadata import cron_proactive_delivery_metadata
@@ -1305,9 +1306,9 @@ def test_gateway_unbound_agent_cron_is_skipped(
         ),
     )
 
-    response = asyncio.run(cron.on_job(job))
+    with pytest.raises(CronJobSkippedError, match="unbound agent cron job"):
+        asyncio.run(cron.on_job(job))
 
-    assert response is None
     bus.publish_outbound.assert_not_awaited()
 
 

tests/cron/test_cron_service.py
@@ -4,7 +4,7 @@ import time
 
 import pytest
 
-from blackcat.cron.service import CronService
+from blackcat.cron.service import CronJobSkippedError, CronService
 from blackcat.cron.types import CronJob, CronPayload, CronSchedule
 
 
@@ -294,6 +294,30 @@ async def test_run_history_records_errors(tmp_path) -> None:
     assert len(loaded.state.run_history) == 1
     assert loaded.state.run_history[0].status == "error"
     assert loaded.state.run_history[0].error == "boom"
+
+
+@pytest.mark.asyncio
+async def test_run_history_records_skipped_jobs(tmp_path) -> None:
+    store_path = tmp_path / "cron" / "jobs.json"
+
+    async def skip(_):
+        raise CronJobSkippedError("missing session binding")
+
+    service = CronService(store_path, on_job=skip)
+    job = service.add_job(
+        name="skip",
+        schedule=CronSchedule(kind="every", every_ms=60_000),
+        message="hello",
+    )
+    await service.run_job(job.id)
+
+    loaded = service.get_job(job.id)
+    assert loaded is not None
+    assert loaded.state.last_status == "skipped"
+    assert loaded.state.last_error == "missing session binding"
+    assert len(loaded.state.run_history) == 1
+    assert loaded.state.run_history[0].status == "skipped"
+    assert loaded.state.run_history[0].error == "missing session binding"
 
 
 @pytest.mark.asyncio