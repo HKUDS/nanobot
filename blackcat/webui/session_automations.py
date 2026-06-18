nanoinfraorg/nanoinfra

fix: show websocket cron jobs in automations

nanobot/webui/session_automations.py
@@ -4,29 +4,26 @@ from __future__ import annotations
 
 from typing import Any, Protocol
 
+from nanobot.cron.automation import is_bound_agent_job
 from nanobot.cron.types import CronJob
 
 
 class _CronServiceLike(Protocol):
-    def list_bound_agent_jobs_for_session(
-        self,
-        session_key: str,
-        *,
-        include_disabled: bool = True,
-    ) -> list[CronJob]: ...
+    def list_jobs(self, *, include_disabled: bool = False) -> list[CronJob]: ...
 
 
-def bound_session_automation_jobs(
+def session_automation_jobs(
     cron_service: _CronServiceLike | None,
     session_key: str,
 ) -> list[CronJob]:
-    """Return agent-turn automation jobs explicitly bound to *session_key*."""
+    """Return user automations attached to the WebUI session."""
     if cron_service is None:
         return []
-    return cron_service.list_bound_agent_jobs_for_session(
-        session_key,
-        include_disabled=True,
-    )
+    return [
+        job
+        for job in cron_service.list_jobs(include_disabled=True)
+        if _matches_webui_session(job, session_key)
+    ]
 
 
 def session_automations_payload(
@@ -34,7 +31,22 @@ def session_automations_payload(
     session_key: str,
 ) -> dict[str, Any]:
     """Return user-created automation jobs attached to a WebUI session."""
-    return {"jobs": serialize_automation_jobs(bound_session_automation_jobs(cron_service, session_key))}
+    return {
+        "jobs": serialize_automation_jobs(session_automation_jobs(cron_service, session_key))
+    }
+
+
+def _matches_webui_session(job: CronJob, session_key: str) -> bool:
+    payload = job.payload
+    if payload.kind != "agent_turn":
+        return False
+    if is_bound_agent_job(job):
+        return payload.session_key == session_key
+    return bool(
+        payload.channel == "websocket"
+        and payload.to
+        and session_key == f"websocket:{payload.to}"
+    )
 
 
 def serialize_automation_jobs(jobs: list[CronJob]) -> list[dict[str, Any]]:

HKUDS/nanobot

blackcat/webui/session_automations.py
@@ -4,29 +4,26 @@ from __future__ import annotations
 
 from typing import Any, Protocol
 
+from blackcat.cron.automation import is_bound_agent_job
 from blackcat.cron.types import CronJob
 
 
 class _CronServiceLike(Protocol):
-    def list_bound_agent_jobs_for_session(
-        self,
-        session_key: str,
-        *,
-        include_disabled: bool = True,
-    ) -> list[CronJob]: ...
+    def list_jobs(self, *, include_disabled: bool = False) -> list[CronJob]: ...
 
 
-def bound_session_automation_jobs(
+def session_automation_jobs(
     cron_service: _CronServiceLike | None,
     session_key: str,
 ) -> list[CronJob]:
-    """Return agent-turn automation jobs explicitly bound to *session_key*."""
+    """Return user automations attached to the WebUI session."""
     if cron_service is None:
         return []
-    return cron_service.list_bound_agent_jobs_for_session(
-        session_key,
-        include_disabled=True,
-    )
+    return [
+        job
+        for job in cron_service.list_jobs(include_disabled=True)
+        if _matches_webui_session(job, session_key)
+    ]
 
 
 def session_automations_payload(
@@ -34,7 +31,22 @@ def session_automations_payload(
     session_key: str,
 ) -> dict[str, Any]:
     """Return user-created automation jobs attached to a WebUI session."""
-    return {"jobs": serialize_automation_jobs(bound_session_automation_jobs(cron_service, session_key))}
+    return {
+        "jobs": serialize_automation_jobs(session_automation_jobs(cron_service, session_key))
+    }
+
+
+def _matches_webui_session(job: CronJob, session_key: str) -> bool:
+    payload = job.payload
+    if payload.kind != "agent_turn":
+        return False
+    if is_bound_agent_job(job):
+        return payload.session_key == session_key
+    return bool(
+        payload.channel == "websocket"
+        and payload.to
+        and session_key == f"websocket:{payload.to}"
+    )
 
 
 def serialize_automation_jobs(jobs: list[CronJob]) -> list[dict[str, Any]]: