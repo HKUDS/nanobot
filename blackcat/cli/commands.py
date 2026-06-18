nanoinfraorg/nanoinfra

refactor: use cron turn naming internally

nanobot/cli/commands.py
@@ -979,12 +979,12 @@ def _run_gateway(
     from nanobot.bus.queue import MessageBus
     from nanobot.bus.runtime_events import RuntimeEventBus
     from nanobot.channels.manager import ChannelManager
-    from nanobot.cron.automation import (
-        AUTOMATION_DEFER_UNTIL_IDLE_META,
-        AUTOMATION_TRIGGER_META,
-        is_bound_agent_job,
-    )
     from nanobot.cron.service import CronService
+    from nanobot.cron.session_turns import (
+        CRON_DEFER_UNTIL_IDLE_META,
+        CRON_TRIGGER_META,
+        is_bound_cron_job,
+    )
     from nanobot.cron.types import CronJob
     from nanobot.providers.factory import build_provider_snapshot, load_provider_snapshot
     from nanobot.providers.image_generation import image_gen_provider_configs
@@ -1093,7 +1093,7 @@ def _run_gateway(
 
         return channel, rest, metadata
 
-    def _automation_prompt_ref(prompt: str) -> dict[str, Any]:
+    def _cron_prompt_ref(prompt: str) -> dict[str, Any]:
         return {
             "id": "cron.agent_turn.reminder",
             "version": 1,
@@ -1110,23 +1110,23 @@ def _run_gateway(
             strip=True,
             message=job.payload.message,
         )
-        prompt_ref = _automation_prompt_ref(prompt)
+        prompt_ref = _cron_prompt_ref(prompt)
         run_id = f"{job.id}:{int(time.time() * 1000)}:{uuid.uuid4().hex[:8]}"
         channel, chat_id, metadata = _bound_session_delivery_context(
             session_key,
             turn_seed=f"cron:{job.id}",
             source_label=job.name,
         )
-        metadata[AUTOMATION_TRIGGER_META] = {
+        metadata[CRON_TRIGGER_META] = {
             "job_id": job.id,
             "job_name": job.name,
             "run_id": run_id,
             "prompt_ref": prompt_ref,
             "persist_content": (
-                f"Scheduled automation triggered: {job.name}\n\n{job.payload.message}"
+                f"Scheduled cron job triggered: {job.name}\n\n{job.payload.message}"
             ),
         }
-        metadata[AUTOMATION_DEFER_UNTIL_IDLE_META] = True
+        metadata[CRON_DEFER_UNTIL_IDLE_META] = True
         run_record_base: dict[str, Any] = {
             "job_id": job.id,
             "job_name": job.name,
@@ -1149,7 +1149,7 @@ def _run_gateway(
         if isinstance(cron_tool, CronTool):
             cron_token = cron_tool.set_cron_context(True)
         try:
-            resp = await agent.submit_automation_turn(
+            resp = await agent.submit_cron_turn(
                 InboundMessage(
                     channel=channel,
                     sender_id="cron",
@@ -1345,7 +1345,7 @@ def _run_gateway(
                 logger.info("Heartbeat: silenced by post-run evaluation")
             return response
 
-        if is_bound_agent_job(job):
+        if is_bound_cron_job(job):
             return await _run_bound_cron_job(job)
 
         reminder_note = (

nanobot/cron/service.py
@@ -14,7 +14,7 @@ from typing import Any, Callable, Coroutine, Literal
 from filelock import FileLock
 from loguru import logger
 
-from nanobot.cron.automation import is_bound_agent_job
+from nanobot.cron.session_turns import is_bound_cron_job
 from nanobot.cron.types import (
     CronJob,
     CronJobState,
@@ -499,17 +499,17 @@ class CronService:
         jobs = store.jobs if include_disabled else [j for j in store.jobs if j.enabled]
         return sorted(jobs, key=lambda j: j.state.next_run_at_ms or float('inf'))
 
-    def list_bound_agent_jobs_for_session(
+    def list_bound_cron_jobs_for_session(
         self,
         session_key: str,
         *,
         include_disabled: bool = True,
     ) -> list[CronJob]:
-        """Return user-created bound automation jobs owned by *session_key*."""
+        """Return user-created bound cron jobs owned by *session_key*."""
         return [
             job
             for job in self.list_jobs(include_disabled=include_disabled)
-            if is_bound_agent_job(job)
+            if is_bound_cron_job(job)
             and job.payload.session_key == session_key
         ]
 

nanobot/session/routing.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from typing import Any, Mapping
 
 from nanobot.bus.events import InboundMessage
-from nanobot.cron.automation import is_automation_turn
+from nanobot.cron.session_turns import is_cron_turn
 from nanobot.session.manager import Session
 from nanobot.session.metadata import SESSION_ROUTING_METADATA_KEY
 
@@ -26,7 +26,7 @@ _ROUTING_METADATA_KEYS = {
 }
 _CHANNEL_ROUTING_METADATA_KEYS = {
     # Feishu needs a message anchor to reply into an existing topic. Other
-    # channels should avoid stale reply anchors for scheduled automation turns.
+    # channels should avoid stale reply anchors for scheduled cron turns.
     "feishu": {"message_id"},
 }
 _SLACK_ROUTING_KEYS = {"channel_type", "thread_ts"}
@@ -74,8 +74,8 @@ def routing_context_for_message(msg: InboundMessage) -> dict[str, Any]:
 
 
 def persist_routing_context(session: Session, msg: InboundMessage) -> bool:
-    """Persist the latest non-automation delivery context for a session."""
-    if is_automation_turn(msg.metadata):
+    """Persist the latest non-cron delivery context for a session."""
+    if is_cron_turn(msg.metadata):
         return False
     context = routing_context_for_message(msg)
     if session.metadata.get(SESSION_ROUTING_METADATA_KEY) == context:

tests/agent/test_loop_save_turn.py
@@ -8,7 +8,7 @@ from nanobot.agent.context import ContextBuilder
 from nanobot.agent.loop import AgentLoop
 from nanobot.bus.events import InboundMessage
 from nanobot.bus.queue import MessageBus
-from nanobot.cron.automation import AUTOMATION_HISTORY_META, AUTOMATION_TRIGGER_META
+from nanobot.cron.session_turns import CRON_HISTORY_META, CRON_TRIGGER_META
 from nanobot.providers.base import LLMResponse
 from nanobot.session.goal_state import GOAL_STATE_KEY
 from nanobot.session.manager import Session, SessionManager
@@ -66,7 +66,7 @@ def test_agent_loop_llm_runtime_reflects_current_provider_and_model(tmp_path: Pa
     assert runtime.model == "next-model"
 
 
-def test_persist_automation_turn_uses_distinct_history_marker(tmp_path: Path) -> None:
+def test_persist_cron_turn_uses_distinct_history_marker(tmp_path: Path) -> None:
     loop = _make_full_loop(tmp_path)
     session = loop.sessions.get_or_create("websocket:auto")
     prompt_ref = {"id": "cron.agent_turn.reminder", "version": 1, "sha256": "abc"}
@@ -76,14 +76,14 @@ def test_persist_automation_turn_uses_distinct_history_marker(tmp_path: Path) ->
             channel="websocket",
             sender_id="cron",
             chat_id="auto",
-            content="Automation: internal prompt",
+            content="Cron job: internal prompt",
             metadata={
-                AUTOMATION_TRIGGER_META: {
+                CRON_TRIGGER_META: {
                     "job_id": "job-1",
                     "job_name": "Daily check",
                     "run_id": "job-1:1",
                     "prompt_ref": prompt_ref,
-                    "persist_content": "Scheduled automation triggered: Daily check",
+                    "persist_content": "Scheduled cron job triggered: Daily check",
                 }
             },
         ),
@@ -92,13 +92,13 @@ def test_persist_automation_turn_uses_distinct_history_marker(tmp_path: Path) ->
 
     assert persisted is True
     message = session.messages[-1]
-    assert message["content"] == "Scheduled automation triggered: Daily check"
-    assert message[AUTOMATION_HISTORY_META] is True
-    assert AUTOMATION_TRIGGER_META not in message
-    assert message["automation_id"] == "job-1"
-    assert message["automation_name"] == "Daily check"
-    assert message["automation_run_id"] == "job-1:1"
-    assert message["automation_prompt_ref"] == prompt_ref
+    assert message["content"] == "Scheduled cron job triggered: Daily check"
+    assert message[CRON_HISTORY_META] is True
+    assert CRON_TRIGGER_META not in message
+    assert message["cron_job_id"] == "job-1"
+    assert message["cron_job_name"] == "Daily check"
+    assert message["cron_run_id"] == "job-1:1"
+    assert message["cron_prompt_ref"] == prompt_ref
 
 
 def test_clean_generated_title_strips_reasoning_tags() -> None:

tests/agent/test_runner_injections.py
@@ -617,12 +617,12 @@ async def test_followup_routed_to_pending_queue(tmp_path):
 
 
 @pytest.mark.asyncio
-async def test_automation_turn_deferred_while_session_active(tmp_path):
-    """Automation turns wait for the active session instead of becoming injections."""
+async def test_cron_turn_deferred_while_session_active(tmp_path):
+    """Cron turns wait for the active session instead of becoming injections."""
     from nanobot.bus.events import InboundMessage
-    from nanobot.cron.automation import (
-        AUTOMATION_DEFER_UNTIL_IDLE_META,
-        AUTOMATION_TRIGGER_META,
+    from nanobot.cron.session_turns import (
+        CRON_DEFER_UNTIL_IDLE_META,
+        CRON_TRIGGER_META,
     )
 
     loop = _make_loop(tmp_path)
@@ -639,15 +639,15 @@ async def test_automation_turn_deferred_while_session_active(tmp_path):
         chat_id="chat-1",
         content="scheduled work",
         metadata={
-            AUTOMATION_TRIGGER_META: {"run_id": "run-1"},
-            AUTOMATION_DEFER_UNTIL_IDLE_META: True,
+            CRON_TRIGGER_META: {"run_id": "run-1"},
+            CRON_DEFER_UNTIL_IDLE_META: True,
         },
         session_key_override=session_key,
     )
     await loop.bus.publish_inbound(msg)
 
     for _ in range(20):
-        if loop._deferred_automation_queues.get(session_key):
+        if loop._cron_turns.deferred_queues.get(session_key):
             break
         await asyncio.sleep(0.05)
 
@@ -656,12 +656,12 @@ async def test_automation_turn_deferred_while_session_active(tmp_path):
 
     assert pending.empty()
     assert loop._dispatch.await_count == 0
-    assert loop._deferred_automation_queues[session_key] == [msg]
+    assert loop._cron_turns.deferred_queues[session_key] == [msg]
 
-    await loop._publish_next_deferred_automation(session_key)
+    await loop._cron_turns.publish_next_deferred(session_key)
     queued = await asyncio.wait_for(loop.bus.consume_inbound(), timeout=0.5)
     assert queued is msg
-    assert session_key not in loop._deferred_automation_queues
+    assert session_key not in loop._cron_turns.deferred_queues
 
 
 @pytest.mark.asyncio

tests/agent/test_task_cancel.py
@@ -10,6 +10,7 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 
 from nanobot.config.schema import AgentDefaults
+from nanobot.session.keys import UNIFIED_SESSION_KEY
 
 _MAX_TOOL_RESULT_CHARS = AgentDefaults().max_tool_result_chars
 
@@ -450,12 +451,12 @@ class TestSubagentAnnounceSessionKey:
         so the result matches the pending queue key."""
         mgr, bus = self._make_mgr()
 
-        origin = {"channel": "telegram", "chat_id": "111", "session_key": "unified:default"}
+        origin = {"channel": "telegram", "chat_id": "111", "session_key": UNIFIED_SESSION_KEY}
         await mgr._announce_result("sub-1", "label", "task", "result", origin, "ok")
 
         msg = await bus.consume_inbound()
-        assert msg.session_key_override == "unified:default"
-        assert msg.session_key == "unified:default"
+        assert msg.session_key_override == UNIFIED_SESSION_KEY
+        assert msg.session_key == UNIFIED_SESSION_KEY
 
     @pytest.mark.asyncio
     async def test_announce_uses_raw_key_in_normal_mode(self):
@@ -505,9 +506,9 @@ class TestSubagentAnnounceSessionKey:
         )
         await mgr._run_subagent(
             "sub-4", "task", "label",
-            {"channel": "telegram", "chat_id": "444", "session_key": "unified:default"},
+            {"channel": "telegram", "chat_id": "444", "session_key": UNIFIED_SESSION_KEY},
             status,
         )
 
         msg = await bus.consume_inbound()
-        assert msg.session_key_override == "unified:default"
+        assert msg.session_key_override == UNIFIED_SESSION_KEY

HKUDS/nanobot

blackcat/cli/commands.py
@@ -979,12 +979,12 @@ def _run_gateway(
     from blackcat.bus.queue import MessageBus
     from blackcat.bus.runtime_events import RuntimeEventBus
     from blackcat.channels.manager import ChannelManager
-    from blackcat.cron.automation import (
-        AUTOMATION_DEFER_UNTIL_IDLE_META,
-        AUTOMATION_TRIGGER_META,
-        is_bound_agent_job,
-    )
     from blackcat.cron.service import CronService
+    from blackcat.cron.session_turns import (
+        CRON_DEFER_UNTIL_IDLE_META,
+        CRON_TRIGGER_META,
+        is_bound_cron_job,
+    )
     from blackcat.cron.types import CronJob
     from blackcat.providers.factory import build_provider_snapshot, load_provider_snapshot
     from blackcat.providers.image_generation import image_gen_provider_configs
@@ -1093,7 +1093,7 @@ def _run_gateway(
 
         return channel, rest, metadata
 
-    def _automation_prompt_ref(prompt: str) -> dict[str, Any]:
+    def _cron_prompt_ref(prompt: str) -> dict[str, Any]:
         return {
             "id": "cron.agent_turn.reminder",
             "version": 1,
@@ -1110,23 +1110,23 @@ def _run_gateway(
             strip=True,
             message=job.payload.message,
         )
-        prompt_ref = _automation_prompt_ref(prompt)
+        prompt_ref = _cron_prompt_ref(prompt)
         run_id = f"{job.id}:{int(time.time() * 1000)}:{uuid.uuid4().hex[:8]}"
         channel, chat_id, metadata = _bound_session_delivery_context(
             session_key,
             turn_seed=f"cron:{job.id}",
             source_label=job.name,
         )
-        metadata[AUTOMATION_TRIGGER_META] = {
+        metadata[CRON_TRIGGER_META] = {
             "job_id": job.id,
             "job_name": job.name,
             "run_id": run_id,
             "prompt_ref": prompt_ref,
             "persist_content": (
-                f"Scheduled automation triggered: {job.name}\n\n{job.payload.message}"
+                f"Scheduled cron job triggered: {job.name}\n\n{job.payload.message}"
             ),
         }
-        metadata[AUTOMATION_DEFER_UNTIL_IDLE_META] = True
+        metadata[CRON_DEFER_UNTIL_IDLE_META] = True
         run_record_base: dict[str, Any] = {
             "job_id": job.id,
             "job_name": job.name,
@@ -1149,7 +1149,7 @@ def _run_gateway(
         if isinstance(cron_tool, CronTool):
             cron_token = cron_tool.set_cron_context(True)
         try:
-            resp = await agent.submit_automation_turn(
+            resp = await agent.submit_cron_turn(
                 InboundMessage(
                     channel=channel,
                     sender_id="cron",
@@ -1345,7 +1345,7 @@ def _run_gateway(
                 logger.info("Heartbeat: silenced by post-run evaluation")
             return response
 
-        if is_bound_agent_job(job):
+        if is_bound_cron_job(job):
             return await _run_bound_cron_job(job)
 
         reminder_note = (

blackcat/cron/service.py
@@ -14,7 +14,7 @@ from typing import Any, Callable, Coroutine, Literal
 from filelock import FileLock
 from loguru import logger
 
-from blackcat.cron.automation import is_bound_agent_job
+from blackcat.cron.session_turns import is_bound_cron_job
 from blackcat.cron.types import (
     CronJob,
     CronJobState,
@@ -499,17 +499,17 @@ class CronService:
         jobs = store.jobs if include_disabled else [j for j in store.jobs if j.enabled]
         return sorted(jobs, key=lambda j: j.state.next_run_at_ms or float('inf'))
 
-    def list_bound_agent_jobs_for_session(
+    def list_bound_cron_jobs_for_session(
         self,
         session_key: str,
         *,
         include_disabled: bool = True,
     ) -> list[CronJob]:
-        """Return user-created bound automation jobs owned by *session_key*."""
+        """Return user-created bound cron jobs owned by *session_key*."""
         return [
             job
             for job in self.list_jobs(include_disabled=include_disabled)
-            if is_bound_agent_job(job)
+            if is_bound_cron_job(job)
             and job.payload.session_key == session_key
         ]
 

blackcat/session/routing.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from typing import Any, Mapping
 
 from blackcat.bus.events import InboundMessage
-from blackcat.cron.automation import is_automation_turn
+from blackcat.cron.session_turns import is_cron_turn
 from blackcat.session.manager import Session
 from blackcat.session.metadata import SESSION_ROUTING_METADATA_KEY
 
@@ -26,7 +26,7 @@ _ROUTING_METADATA_KEYS = {
 }
 _CHANNEL_ROUTING_METADATA_KEYS = {
     # Feishu needs a message anchor to reply into an existing topic. Other
-    # channels should avoid stale reply anchors for scheduled automation turns.
+    # channels should avoid stale reply anchors for scheduled cron turns.
     "feishu": {"message_id"},
 }
 _SLACK_ROUTING_KEYS = {"channel_type", "thread_ts"}
@@ -74,8 +74,8 @@ def routing_context_for_message(msg: InboundMessage) -> dict[str, Any]:
 
 
 def persist_routing_context(session: Session, msg: InboundMessage) -> bool:
-    """Persist the latest non-automation delivery context for a session."""
-    if is_automation_turn(msg.metadata):
+    """Persist the latest non-cron delivery context for a session."""
+    if is_cron_turn(msg.metadata):
         return False
     context = routing_context_for_message(msg)
     if session.metadata.get(SESSION_ROUTING_METADATA_KEY) == context:

tests/agent/test_loop_save_turn.py
@@ -8,7 +8,7 @@ from blackcat.agent.context import ContextBuilder
 from blackcat.agent.loop import AgentLoop
 from blackcat.bus.events import InboundMessage
 from blackcat.bus.queue import MessageBus
-from blackcat.cron.automation import AUTOMATION_HISTORY_META, AUTOMATION_TRIGGER_META
+from blackcat.cron.session_turns import CRON_HISTORY_META, CRON_TRIGGER_META
 from blackcat.providers.base import LLMResponse
 from blackcat.session.goal_state import GOAL_STATE_KEY
 from blackcat.session.manager import Session, SessionManager
@@ -66,7 +66,7 @@ def test_agent_loop_llm_runtime_reflects_current_provider_and_model(tmp_path: Pa
     assert runtime.model == "next-model"
 
 
-def test_persist_automation_turn_uses_distinct_history_marker(tmp_path: Path) -> None:
+def test_persist_cron_turn_uses_distinct_history_marker(tmp_path: Path) -> None:
     loop = _make_full_loop(tmp_path)
     session = loop.sessions.get_or_create("websocket:auto")
     prompt_ref = {"id": "cron.agent_turn.reminder", "version": 1, "sha256": "abc"}
@@ -76,14 +76,14 @@ def test_persist_automation_turn_uses_distinct_history_marker(tmp_path: Path) ->
             channel="websocket",
             sender_id="cron",
             chat_id="auto",
-            content="Automation: internal prompt",
+            content="Cron job: internal prompt",
             metadata={
-                AUTOMATION_TRIGGER_META: {
+                CRON_TRIGGER_META: {
                     "job_id": "job-1",
                     "job_name": "Daily check",
                     "run_id": "job-1:1",
                     "prompt_ref": prompt_ref,
-                    "persist_content": "Scheduled automation triggered: Daily check",
+                    "persist_content": "Scheduled cron job triggered: Daily check",
                 }
             },
         ),
@@ -92,13 +92,13 @@ def test_persist_automation_turn_uses_distinct_history_marker(tmp_path: Path) ->
 
     assert persisted is True
     message = session.messages[-1]
-    assert message["content"] == "Scheduled automation triggered: Daily check"
-    assert message[AUTOMATION_HISTORY_META] is True
-    assert AUTOMATION_TRIGGER_META not in message
-    assert message["automation_id"] == "job-1"
-    assert message["automation_name"] == "Daily check"
-    assert message["automation_run_id"] == "job-1:1"
-    assert message["automation_prompt_ref"] == prompt_ref
+    assert message["content"] == "Scheduled cron job triggered: Daily check"
+    assert message[CRON_HISTORY_META] is True
+    assert CRON_TRIGGER_META not in message
+    assert message["cron_job_id"] == "job-1"
+    assert message["cron_job_name"] == "Daily check"
+    assert message["cron_run_id"] == "job-1:1"
+    assert message["cron_prompt_ref"] == prompt_ref
 
 
 def test_clean_generated_title_strips_reasoning_tags() -> None:

tests/agent/test_runner_injections.py
@@ -617,12 +617,12 @@ async def test_followup_routed_to_pending_queue(tmp_path):
 
 
 @pytest.mark.asyncio
-async def test_automation_turn_deferred_while_session_active(tmp_path):
-    """Automation turns wait for the active session instead of becoming injections."""
+async def test_cron_turn_deferred_while_session_active(tmp_path):
+    """Cron turns wait for the active session instead of becoming injections."""
     from blackcat.bus.events import InboundMessage
-    from blackcat.cron.automation import (
-        AUTOMATION_DEFER_UNTIL_IDLE_META,
-        AUTOMATION_TRIGGER_META,
+    from blackcat.cron.session_turns import (
+        CRON_DEFER_UNTIL_IDLE_META,
+        CRON_TRIGGER_META,
     )
 
     loop = _make_loop(tmp_path)
@@ -639,15 +639,15 @@ async def test_automation_turn_deferred_while_session_active(tmp_path):
         chat_id="chat-1",
         content="scheduled work",
         metadata={
-            AUTOMATION_TRIGGER_META: {"run_id": "run-1"},
-            AUTOMATION_DEFER_UNTIL_IDLE_META: True,
+            CRON_TRIGGER_META: {"run_id": "run-1"},
+            CRON_DEFER_UNTIL_IDLE_META: True,
         },
         session_key_override=session_key,
     )
     await loop.bus.publish_inbound(msg)
 
     for _ in range(20):
-        if loop._deferred_automation_queues.get(session_key):
+        if loop._cron_turns.deferred_queues.get(session_key):
             break
         await asyncio.sleep(0.05)
 
@@ -656,12 +656,12 @@ async def test_automation_turn_deferred_while_session_active(tmp_path):
 
     assert pending.empty()
     assert loop._dispatch.await_count == 0
-    assert loop._deferred_automation_queues[session_key] == [msg]
+    assert loop._cron_turns.deferred_queues[session_key] == [msg]
 
-    await loop._publish_next_deferred_automation(session_key)
+    await loop._cron_turns.publish_next_deferred(session_key)
     queued = await asyncio.wait_for(loop.bus.consume_inbound(), timeout=0.5)
     assert queued is msg
-    assert session_key not in loop._deferred_automation_queues
+    assert session_key not in loop._cron_turns.deferred_queues
 
 
 @pytest.mark.asyncio

tests/agent/test_task_cancel.py
@@ -10,6 +10,7 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 
 from blackcat.config.schema import AgentDefaults
+from blackcat.session.keys import UNIFIED_SESSION_KEY
 
 _MAX_TOOL_RESULT_CHARS = AgentDefaults().max_tool_result_chars
 
@@ -450,12 +451,12 @@ class TestSubagentAnnounceSessionKey:
         so the result matches the pending queue key."""
         mgr, bus = self._make_mgr()
 
-        origin = {"channel": "telegram", "chat_id": "111", "session_key": "unified:default"}
+        origin = {"channel": "telegram", "chat_id": "111", "session_key": UNIFIED_SESSION_KEY}
         await mgr._announce_result("sub-1", "label", "task", "result", origin, "ok")
 
         msg = await bus.consume_inbound()
-        assert msg.session_key_override == "unified:default"
-        assert msg.session_key == "unified:default"
+        assert msg.session_key_override == UNIFIED_SESSION_KEY
+        assert msg.session_key == UNIFIED_SESSION_KEY
 
     @pytest.mark.asyncio
     async def test_announce_uses_raw_key_in_normal_mode(self):
@@ -505,9 +506,9 @@ class TestSubagentAnnounceSessionKey:
         )
         await mgr._run_subagent(
             "sub-4", "task", "label",
-            {"channel": "telegram", "chat_id": "444", "session_key": "unified:default"},
+            {"channel": "telegram", "chat_id": "444", "session_key": UNIFIED_SESSION_KEY},
             status,
         )
 
         msg = await bus.consume_inbound()
-        assert msg.session_key_override == "unified:default"
+        assert msg.session_key_override == UNIFIED_SESSION_KEY